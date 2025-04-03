d4e4323d14f562ce2e5929864f1b0a130eb57393
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-03 22:20:22 +0300

    Add social media icona

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 import { Clock, MapPin } from "lucide-react"
+import twitter from '../svgs/twitter.svg'
+import facebook from '../svgs/facebook.svg'
+import instagram from '../svgs/instagram.svg'
+import linkedin from '../svgs/linkedin.svg'
+    
 
 export default function Footer() {
     return (
@@ -12,12 +18,42 @@ export default function Footer() {
                         <p className='text-[18px] text-center'>Monday - Friday</p>
                         <p className='text-[18px] text-center'>8 AM - 1PM (Saturday 8AM - 1PM)</p>
                     </div>
-                    <div className='border border-white w-[328px] h-[144px] flex justify-center'>
+                    <div className='border border-white w-[328px] h-[144px] flex flex-col justify-center'>
                         <MapPin size={31} className='text-red'/>
                         <p className='text-[18px] text-center'>Garden estate, along Garden estate rd Nairobi, Kenya</p>
                     </div>
-                    <div className='border border-white w-[328px] h-[144px] flex justify-center'>
+                    <div className='border border-white w-[328px] h-[144px] flex flex-col justify-center'>
                         <p className='text-[18px] text-center'>Visit Us on</p>
+                        <div className='flex flex-row w-full items-center justify-between'>
+                            <Image
+                                src={twitter}
+                                alt='Twitter Icon'
+                                width={48}
+                                height={48}
+                                className='hover:border hover:border-white'
+                            />
+                            <Image
+                                src={facebook}
+                                alt='Facebook Icon'
+                                width={48}
+                                height={48}
+                                className='hover:border hover:border-white'
+                            />
+                            <Image
+                                src={instagram}
+                                alt='Instagram Icon'
+                                width={48}
+                                height={48}
+                                className='hover:border hover:border-white'
+                            />
+                            <Image
+                                src={linkedin}
+                                alt='Linkedin Icon'
+                                width={48}
+                                height={48}
+                                className='hover:border hover:border-white'
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
