476305dbeac2a66cdd36c7999970b4a04b12be30
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-03 14:35:58 +0300

    Remove file extension from imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
-import Header from "./components/header.tsx";
-import Footer from "./components/footer.tsx";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import "./globals.css";
 
 // const geistSans = Geist({
