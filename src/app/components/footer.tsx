36b3e0b7d11f49d21b2585935d455e06cec3886e
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-03 22:54:21 +0300

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -13,16 +13,16 @@ export default function Footer() {
             {/* Header for large and medium screens*/}
             <div className='hidden lg:flex lg:flex-col text-white md:flex md:fex-col items-center px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#252865]'>
                 <div className='flex flex-row justify-between'>
-                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center'>
+                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center justify-between'>
                         <Clock size={31} className='text-white'/>
                         <p className='text-[18px] text-center'>Monday - Friday</p>
                         <p className='text-[18px] text-center'>8 AM - 1PM (Saturday 8AM - 1PM)</p>
                     </div>
-                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center'>
+                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center justify-between'>
                         <MapPin size={31} className='text-red'/>
                         <p className='text-[18px] text-center'>Garden estate, along Garden estate rd Nairobi, Kenya</p>
                     </div>
-                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center'>
+                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center justify-between'>
                         <p className='text-[18px] text-center'>Visit Us on</p>
                         <div className='flex flex-row w-full items-center justify-between'>
                             <Image
@@ -30,31 +30,36 @@ export default function Footer() {
                                 alt='Twitter Icon'
                                 width={48}
                                 height={48}
-                                className='hover:border hover:border-white hover:rounded-full hover:p-2'
+                                className='hover:border hover:border-white hover:rounded-full hover:p-1'
                             />
                             <Image
                                 src={facebook}
                                 alt='Facebook Icon'
                                 width={48}
                                 height={48}
-                                className='hover:border hover:border-white hover:rounded-full hover:p-2'
+                                className='hover:border hover:border-white hover:rounded-full hover:p-1'
                             />
                             <Image
                                 src={instagram}
                                 alt='Instagram Icon'
                                 width={48}
                                 height={48}
-                                className='hover:border hover:border-white hover:rounded-full hover:p-2'
+                                className='hover:border hover:border-white hover:rounded-full hover:p-1'
                             />
                             <Image
                                 src={linkedin}
                                 alt='Linkedin Icon'
                                 width={48}
                                 height={48}
-                                className='hover:border hover:border-white hover:rounded-full hover:p2'
+                                className='hover:border hover:border-white hover:rounded-full hover:p-1'
                             />
                         </div>
                     </div>
+                </div>
+                <div className='flex flex-row justify-between'>
+                    <p className='text-[18px] text-center'>Alver Power Systems Ltd &copy; 2022. All rights reserved.</p>
+                    <p className='text-[18px] text-center hover:underline'>Privacy</p>
+                    <p className='text-[18px] text-center hover:underline'>Terms</p>
                 </div>
             </div>
             {/* Header for small screens */}
