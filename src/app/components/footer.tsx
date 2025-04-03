74925dc25d05f5a6fc4f3e599ac67bbd0cca0883
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-03 22:58:45 +0300

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -11,18 +11,18 @@ export default function Footer() {
     return (
         <footer className='w-full mb-4 flex items-center justify-center'>
             {/* Header for large and medium screens*/}
-            <div className='hidden lg:flex lg:flex-col text-white md:flex md:fex-col items-center px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#252865]'>
+            <div className='hidden lg:flex lg:flex-col text-white md:flex md:fex-col items-center justify-between px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#252865]'>
                 <div className='flex flex-row justify-between'>
-                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center justify-between'>
+                    <div className='w-[328px] h-[144px] flex flex-col items-center justify-between'>
                         <Clock size={31} className='text-white'/>
                         <p className='text-[18px] text-center'>Monday - Friday</p>
                         <p className='text-[18px] text-center'>8 AM - 1PM (Saturday 8AM - 1PM)</p>
                     </div>
-                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center justify-between'>
+                    <div className='w-[328px] h-[144px] flex flex-col items-center justify-between'>
                         <MapPin size={31} className='text-red'/>
                         <p className='text-[18px] text-center'>Garden estate, along Garden estate rd Nairobi, Kenya</p>
                     </div>
-                    <div className='border border-white w-[328px] h-[144px] flex flex-col items-center justify-between'>
+                    <div className='w-[328px] h-[144px] flex flex-col items-center justify-between'>
                         <p className='text-[18px] text-center'>Visit Us on</p>
                         <div className='flex flex-row w-full items-center justify-between'>
                             <Image
@@ -56,7 +56,7 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-row justify-between'>
+                <div className='w-full flex flex-row items-center justify-between'>
                     <p className='text-[18px] text-center'>Alver Power Systems Ltd &copy; 2022. All rights reserved.</p>
                     <p className='text-[18px] text-center hover:underline'>Privacy</p>
                     <p className='text-[18px] text-center hover:underline'>Terms</p>
