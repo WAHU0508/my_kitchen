a6674ba12ee3db9f22bbc06985af120b4448a778
WAHU0508 <sharongikenye@gmail.com>
2025-04-03 14:32:05 +0300

    Comment out unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/header.tsx";
 import Footer from "./components/footer.tsx";
 import "./globals.css";
