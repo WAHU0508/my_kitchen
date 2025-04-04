32804ce8ac1652c7d4568b401000391a1ba3b5e6
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-04 08:42:05 +0300

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { Clock, MapPin } from "lucide-react"
+import mapPin from "../svgs/mapPin.svg"
 import twitter from '../svgs/twitter.svg'
 import facebook from '../svgs/facebook.svg';
 import instagram from '../svgs/instagram.svg';
@@ -14,16 +15,21 @@ export default function Footer() {
             {/* Header for large and medium screens*/}
             <div className='hidden lg:flex lg:flex-col text-white md:flex md:fex-col items-center justify-between px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#252865]'>
                 <div className='flex flex-row justify-between gap-[50px]'>
-                    <div className='w-[328px] h-[144px] flex flex-col items-center justify-between'>
+                    <div className='w-[321px] h-[137px] flex flex-col items-center justify-between'>
                         <Clock size={31} className='text-white'/>
                         <p className='text-[18px] text-center'>Monday - Friday</p>
                         <p className='text-[18px] text-center'>8 AM - 1PM (Saturday 8AM - 1PM)</p>
                     </div>
-                    <div className='w-[328px] h-[144px] flex flex-col items-center justify-between'>
-                        <MapPin size={31} className='text-red'/>
+                    <div className='w-[386px] h-[144px] flex flex-col items-center justify-between'>
+                        <Image
+                            src={mapPin}
+                            width={31}
+                            height={31}
+                            alt='Map Pin'
+                        />
                         <p className='text-[18px] text-center'>Garden estate, along Garden estate rd Nairobi, Kenya</p>
                     </div>
-                    <div className='w-[328px] h-[144px] flex flex-col items-center justify-between'>
+                    <div className='w-[348px] h-[144px] flex flex-col items-center justify-between'>
                         <p className='text-[18px] text-center'>Visit Us on</p>
                         <div className='flex flex-row w-full items-center justify-between'>
                             <Image
