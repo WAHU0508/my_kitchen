c35eda0691c25408bf1dcf9f85b1cd8b3ee2cb85
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-04 11:35:37 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <div>Home Page</div>
+    <section className='w-full flex items-center justify-center'>
+      <div className='border border-black lg:p-4 lg:w-[1300px]'>
+        Home Page
+      </div>
+    </section>
   );
 }
