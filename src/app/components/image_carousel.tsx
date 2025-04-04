040b8de7526e21feee34ab5bdc972057f3a5e065
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-04 15:13:58 +0300

    Update image_carousel.tsx

--- src/app/components/image_carousel.tsx
+++ src/app/components/image_carousel.tsx
@@ -1,4 +1,6 @@
 'use client';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import React from 'react';
 import Image from 'next/image';
 import electricalsImg from '../assets/electricals.png'
