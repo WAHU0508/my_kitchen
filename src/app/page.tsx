e12633567f719a9b5a2c5f1cf538193814e65413
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-05 11:32:26 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -156,7 +156,7 @@ We specialize in high-quality electrical installations, solar energy solutions a
                 <div className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] inset-0 border-[2px] border-[#252865] rounded-[15px] blur-[8.25px]"></div>
                 {/* Rectangle 8 - foreground box */}
                 <button className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2">
-                  Learn More
+                  Explore our products
                   <Image
                     src={externalLink}
                     alt='Explore'
@@ -166,7 +166,6 @@ We specialize in high-quality electrical installations, solar energy solutions a
                   />
                 </button>
               </div>
-            </div>
           </div>
           
           {/*Next Section*/}
