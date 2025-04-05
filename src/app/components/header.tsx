747f7f97041474e190ec3e4a37e0447ef0c7cece
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-05 17:55:30 +0300

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -18,9 +18,12 @@ export default function Header() {
     // Disable scroll when modal is open
     useEffect(() => {
         if (isOpen) {
-            document.body.style.overflow = 'hidden'; // Disable scrolling
+            document.body.style.overflow = 'hidden';
+            document.body.style.position = 'fixed';
+            document.body.style.width = '100%';
         } else {
-            document.body.style.overflow = 'auto'; // Re-enable scrolling
+            document.body.style.overflow = 'auto';
+            document.body.style.position = 'static';
         }
     }, [isOpen]);
 
