4c8c6cb3d30c38f07c70438b1103593e300e6d27
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-05 12:53:18 +0300

    Update project_card1.tsx

--- src/app/components/project_card1.tsx
+++ src/app/components/project_card1.tsx
@@ -5,7 +5,7 @@ import solargen from '../assets/solargen.png'
 
 export default function ProjectCard() {
   return (
-    <div className='w-[300px] h-[259px] lg:w-[386px] lg:h-[379px] bg-[rgba(50,101,37,0.47)] opacity-[0.37] border border-[#D9D9D9] shadow-[0px_4px_4px_5px_rgba(0,0,0,0.25)]'>
+    <div className='w-[300px] h-[259px] lg:w-[386px] lg:h-[379px] bg-[rgba(50,101,37,0.47)] border border-[#D9D9D9] shadow-[0px_4px_4px_5px_rgba(0,0,0,0.25)]'>
       <div className='w-[300px] h-[105px] lg:w-[379px] lg:h-[133px] mt-[10px] lg:mt-[20px]'>
         <Image
           src={solargen}
@@ -15,7 +15,7 @@ export default function ProjectCard() {
           className='w-[300px] h-[105px] lg:w-[379px] lg:h-[133px]'
         />
       </div>
-      <p className='text-[20px] lg:text-[24px] mt-[20px] text-black'>Solargen Technologies</p>
+      <p className='text-[20px] lg:text-[24px] mt-[20px] text-[#000000]'>Solargen Technologies</p>
       <div className='flex flex-row gap-2'>
         <Image
           src={mapPin}
@@ -23,9 +23,9 @@ export default function ProjectCard() {
           width={12}
           height={12}
         />
-        <p className='text-[16px] text-black'>Nairobi</p>
-        <div className="w-[158.15px] h-0 left-[20px] lg:left-[34.92px] top-[60px] border-t-[5px] border-[#252865]"></div>
+        <p className='text-[16px] text-[#000000]'>Nairobi</p>
       </div>
+      <div className="w-[158.15px] h-0 left-[20px] lg:left-[34.92px] top-[60px] border-t-[5px] border-[#252865]"></div>
     </div>
   )
 }
