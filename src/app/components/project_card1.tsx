d0b605eaf43894f97f7bdae24b84e16c7b322fbd
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-05 14:36:41 +0300

    Update project_card1.tsx

--- src/app/components/project_card1.tsx
+++ src/app/components/project_card1.tsx
@@ -5,7 +5,7 @@ import solargen from '../assets/solargen.png'
 
 export default function ProjectCard() {
   return (
-    <div className='w-[300px] h-[259px] lg:w-[386px] lg:h-[379px] bg-[rgba(50,101,37,0.47)] border border-[#D9D9D9] shadow-[0px_4px_4px_5px_rgba(0,0,0,0.25)]'>
+    <div className='w-[300px] h-[270px] lg:w-[386px] lg:h-[379px] bg-[rgba(50,101,37,0.47)] border border-[#D9D9D9] shadow-[0px_4px_4px_5px_rgba(0,0,0,0.25)]'>
       <div className='w-[300px] h-[105px] lg:w-[379px] lg:h-[133px] mt-[10px] lg:mt-[20px]'>
         <Image
           src={solargen}
