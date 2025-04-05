8d44716d0a94fc6d14b8f84363cce26f7e9ab468
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-05 15:51:09 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ We specialize in high-quality electrical installations, solar energy solutions a
             {/* Rectangle 9 - blurred background box */}
             <div className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] inset-0 border-[2px] border-[#252865] rounded-[15px] blur-[8.25px]"></div>
             {/* Rectangle 8 - foreground box */}
-            <button className="absolute hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2"
+            <button className="absolute text-black hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2"
                     onClick={products_and_services}
               >
               Explore our products
@@ -91,7 +91,7 @@ We specialize in high-quality electrical installations, solar energy solutions a
             {/* Rectangle 9 - blurred background box */}
             <div className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] inset-0 border-[2px] border-[#252865] rounded-[15px] blur-[8.25px]"></div>
             {/* Rectangle 8 - foreground box */}
-            <button className="absolute hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center justify-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2">
+            <button className="absolute text-black hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center justify-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2">
               Get a Quote
               <Image
                 src={externalLink}
@@ -126,7 +126,7 @@ We specialize in high-quality electrical installations, solar energy solutions a
                 {/* Rectangle 9 - blurred background box */}
                 <div className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] inset-0 border-[2px] border-[#252865] rounded-[15px] blur-[8.25px]"></div>
                 {/* Rectangle 8 - foreground box */}
-                <button className="absolute hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center justify-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2"
+                <button className="absolute text-black hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center justify-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2"
                         onClick={about_us}
                   >
                   Learn More
@@ -168,7 +168,7 @@ We specialize in high-quality electrical installations, solar energy solutions a
                 {/* Rectangle 9 - blurred background box */}
                 <div className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] inset-0 border-[2px] border-[#252865] rounded-[15px] blur-[8.25px]"></div>
                 {/* Rectangle 8 - foreground box */}
-                <button className="absolute hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2"
+                <button className="absolute text-black hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2"
                          onClick={products_and_services}
                   >
                   Explore our products
@@ -247,7 +247,7 @@ We specialize in high-quality electrical installations, solar energy solutions a
                 {/* Rectangle 9 - blurred background box */}
                 <div className="absolute w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] inset-0 border-[2px] border-[#252865] rounded-[15px] blur-[8.25px]"></div>
                 {/* Rectangle 8 - foreground box */}
-                <button className="absolute hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center justify-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2">
+                <button className="absolute text-black hover:bg-[#252865] hover:text-white active:bg-[#252865] active:text-white w-[123px] h-[23px] lg:w-[290px] lg:h-[46px] md:w-[290px] md:h-[46px] text-[10px] lg:text-[24px] md:text-[24px] inset-0 border-[2px] border-[#252865] rounded-[15px] flex flex-row items-center justify-center lg:items-center lg:justify-center md:items-center md:justify-center lg:gap-2 md:gap-2">
                   See More
                   <Image
                     src={externalLink}
