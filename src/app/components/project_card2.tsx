72db205a3512832a126e8b49d80493574b71669c
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-05 12:28:24 +0300

    Create project_card2.tsx

---
+++ src/app/components/project_card2.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ProjectCard() {
+  return (
+    <div className='w-[300px] h-[259px] lg:w-[386px] lg:h-[379px] bg-[rgba(50,101,37,0.47)] opacity-[0.37] border border-[#D9D9D9] shadow-[0px_4px_4px_5px_rgba(0,0,0,0.25)]'>
+      <div className='w-[300px] h-[105px] lg:w-[379px] lg:h-[133px] mt-[10px] lg:mt-[20px]'>
+      </div>
+
+      <p className='text-[20px] lg:text-[24px] mt-[20px]'>Solargen Technologies</p>
+    </div>
+  )
+}
