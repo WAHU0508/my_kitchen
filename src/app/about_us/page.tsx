25c50b89e129a147eeef3b2a1266fada9eec4e08
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 15:27:10 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import coverImg from '../assets/sivacon.jpg';
 import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default function AboutUsPage() {
   return (
     <section className="relative w-full flex flex-col items-center justify-center overflow-hidden">
 
       {/* Background Image */}
-      <div className="relative w-[1440px] h-[700px] z-0">
+      <div className="relative w-[1440px] h-[550px] z-0">
         <Image
           src={coverImg}
           alt="Cover Image"
           fill
-          className="object-cover"
+          className="object-cover opacity-50"
           priority
         />
       </div>
