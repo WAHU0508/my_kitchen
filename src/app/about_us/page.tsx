1be8cc8327d00d1ef82414e87fb36ab91b8eca32
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 16:33:29 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -7,9 +7,9 @@ import Footer from '../components/footer';
 export default function AboutUsPage() {
   return (
     <section className="relative w-full flex flex-col items-center overflow-x-hidden">
-      <div className='hidden md:block'>
+      <div className='hidden md:block w-full flex flex-col items-center'>
         {/* Background Image with header and quote overlay */}
-        <div className="relative w-[1440px] h-[550px]">
+        <div className="relative w-full lg:w-[1440px] h-[550px]">
           <Image
             src={coverImg}
             alt="Cover Image"
@@ -19,12 +19,12 @@ export default function AboutUsPage() {
           />
       
           {/* Header inside image container (or move outside if needed) */}
-          <div className="absolute top-4 left-0 w-full z-20">
+          <div className="absolute top-4 left-0 w-full lg:z-20">
             <Header />
           </div>
       
           {/* Quote overlay */}
-          <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 z-10 text-center">
+          <div className="absolute bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
             <p className="text-white font-semibold text-[32px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
               Driven by Excellence, Energized by Trust.
             </p>
@@ -32,7 +32,7 @@ export default function AboutUsPage() {
         </div>
       
         {/* Main content */}
-        <div className="border border-black max-w-[1300px] px-6 md:px-[100px] flex flex-col mt-10">
+        <div className="md:w-full lg:max-w-[1300px] px-6 md:px-[100px] flex flex-col mt-10">
           <div className="text-black text-[24px] mb-4">Our journey</div>
           <div className="flex flex-row items-start gap-6">
             <div className="w-[10px] h-[50px] bg-[#FF0105]"></div>
