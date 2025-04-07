81c8d6a21a4cd518d6aea39d2571b59c14c7227c
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 09:44:20 +0300

    Create get_quote_modal.tsx

---
+++ src/app/components/get_quote_modal.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Modal() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-black/30 z-50">
+           <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
+             {/* Modal Header with close button */}
+             <div className="flex justify-between items-center">
+               <h2 className="text-xl font-bold">Get A Quote</h2>
+               <button
+                 className="text-xl text-gray-600 hover:text-gray-800"
+                 onClick={closeModal}
+               >
+                 &times; {/* "X" to close */}
+               </button>
+             </div>
+ 
+             {/* Modal Content */}
+             <div className="mt-4">
+               <p>Development in Progress</p>
+             </div>
+ 
+             {/* Modal Footer */}
+             {/*<div className="mt-4 flex justify-end">
+               <button
+                 className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+                 onClick={closeModal}
+               >
+                 Close
+               </button>
+             </div>*/}
+           </div>
+         </div>
+  )
+}
