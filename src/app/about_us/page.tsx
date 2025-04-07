2408063bc0dedd04ae7eeba317d03c5815243923
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 16:08:30 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -6,7 +6,7 @@ import Footer from '../components/footer';
 
 export default function AboutUsPage() {
   return (
-    <section className="relative w-full flex flex-col items-center justify-center overflow-hidden">
+    <section className="relative w-full flex flex-col items-center justify-center">
 
       {/* Background Image */}
       <div className="relative w-[1440px] h-[550px] z-0">
@@ -23,7 +23,7 @@ export default function AboutUsPage() {
       <div className="absolute top-2 z-10">
         <Header />
       </div>
-      <div className='border-border-white flex items-center absolute lg:w-[1300px]'>
+      <div className='border border-white flex items-center absolute lg:w-[1300px]'>
         <p className='text-white font-semibold ml-[150px] text-[50px] w-[600px]'>Driven by Excellence, Energized by Trust.</p>
       </div>
       
@@ -31,10 +31,10 @@ export default function AboutUsPage() {
         <div className='text-black lg:text-[24px]'>Our journey</div>
         <div className='flex flex-row'>
           <div className='w-[10px] h-[50px] bg-[#FF0105]'></div>
-{/*           <div className='lg:ml-[100px] text-black text-[16px]'>Alver Power Systems is a company that was founded and registered in 2022 in accordance with the Kenya Company Act rules, however it has been in operation as a sole proprietor since 2017. Our goal is to offer cost-effective power solutions and sustainable, renewable energy-saving solutions that are both inexpensive and environmentally beneficial for consumers, businesses, and institutions.
+          <div className='lg:ml-[100px] text-black text-[16px]'>Alver Power Systems is a company that was founded and registered in 2022 in accordance with the Kenya Company Act rules, however it has been in operation as a sole proprietor since 2017. Our goal is to offer cost-effective power solutions and sustainable, renewable energy-saving solutions that are both inexpensive and environmentally beneficial for consumers, businesses, and institutions.
 In order to consistently produce high-quality electrical solutions that perform and provide protection, we at Alver Power System have formed our company around understanding the needs and expectations of our customers. Our work is done with no exceptions, and it shows in the final outcome. Every project is handled equally, whether it involves industrial buildings, residential structures, or commercial and retail fit-outs. Experienced project managers, licensed electricians, and office-based support staff guarantee the effective completion of your contract. Our consumers' faith and trust in using our services has contributed to our success. We are committed to delivering high-quality services on time and under budget, keeping everyone safe, and working together with all parties. 
             A focus on quality control and customer service means that a significant amount of our business originates from clients who frequently use our services.
-          </div> */}
+          </div>
         </div>
       </div>
     </section>
