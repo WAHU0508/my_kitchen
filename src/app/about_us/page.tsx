99693e72745edf1d986964cc1bda8b59a71f72f8
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 15:14:53 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -8,17 +8,16 @@ export default function AboutUsPage() {
     <section className='relative top-0 w-full flex flex-col items-center justify-center overflow-hidden'>
 
       {/* Background Image */}
-      <div className='absolute top-0 left-0 w-[1440px] h-[700px] z-0'>
+      <div className='absolute top-0 left-0 w-[1440px] h-[700px]'>
         <Image
           src={coverImg}
           alt='Cover Image'
-          fill
           className='object-cover'
         />
       </div>
 
       {/* Header in front */}
-      <div className='relative z-10 w-full'>
+      <div className='relative w-full'>
         <Header />
       </div>
       
