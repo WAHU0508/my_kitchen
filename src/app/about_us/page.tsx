269f0eda5ad6c92c80d431acc03965396c4e706d
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 15:58:27 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -26,11 +26,11 @@ export default function AboutUsPage() {
       <div className='absolute lg:w-[1300px]'>
         <p className='text-white font-semibold ml-[150px] text-[50px] w-[600px]'>Driven by Excellence, Energized by Trust.</p>
       </div>
-      <div className='lg:w-[1300px] flex flex-col'>
+      <div className='lg:w-[1300px] px-[150px] flex flex-col'>
         <div className='text-black lg:text-[24px]'>Our journey</div>
         <div className='flex flex-row'>
           <div className='w-[5px] h-[50px] bg-[#FF0105]'></div>
-          <div>About us</div>
+          <div className='lg:ml-[100px] text-black text-[16px]'>About us</div>
         </div>
       </div>
     </section>
