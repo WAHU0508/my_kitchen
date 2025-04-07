810bbad6d80b7eb34efdd20cdf41a89871f9ad0f
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 12:20:07 +0300

    Update about_us  page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function AboutUsPage() {
   return (
-    <div>About Us page</div>
+    <section className='w-full flex items-center justify-center'>
+      <div className='w-full flex items-center justify-center bg-[#CACACA]'>
+        <div className='md:w-full w-full lg:w-[1300px] lg:h-[141px] h-[102px] md:h-[141px]'>
+          <p className='lg:text-[32px] text-semibold text-black lg:ml-[90px] md:w-[327px]  lg:w-[327px] w-[214px]'>About Us - Alver Power Systems.Ltd</p>
+        </div>
+      </div>
+      <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>
+        
+      </div>
+    </section>
   )
 }
