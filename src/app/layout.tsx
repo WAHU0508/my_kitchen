d83fbacc7173618aa282957d499589ad42dbb546
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 14:54:23 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
-import Header from "./components/header";
-import Footer from "./components/footer";
+// import Header from "./components/header";
+// import Footer from "./components/footer";
 import "./globals.css";
 
 // const geistSans = Geist({
@@ -28,9 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        <Header />
+{/*         <Header /> */}
         <main className="flex-grow">{children}</main>
-        <Footer />
+{/*         <Footer /> */}
       </body>
     </html>
   );
