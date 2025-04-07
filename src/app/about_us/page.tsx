2be977a09678eb28301762dbb6eea060135c8981
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 14:41:49 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -5,11 +5,11 @@ import coverImg from '../assets/sivacon.jpg'
 export default function AboutUsPage() {  
   return (
     <section className='top-0 w-full flex flex-col items-center justify-center'>
-      <div className='w-[1440px] h-[300px]'>
+      <div className='w-[1440px]'>
         <Image
           src={coverImg}
           alt='Cover Image'
-          width={1200}
+          width={1440}
           height={300}
         />
       </div>
