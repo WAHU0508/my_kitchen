9a3c4094889a603b336f1fe59e84d7fa83abedcb
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 14:27:30 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -5,7 +5,7 @@ import coverImg from '../assets/switchboard_3_.png'
 export default function AboutUsPage() {  
   return (
     <section className='absolute w-full flex flex-col items-center justify-center'>
-      <div className='w-[1440px] h-[600px]'>
+      <div className='w-[1440px] h-[300px]'>
         <Image
           src={coverImg}
           alt='Cover Image'
