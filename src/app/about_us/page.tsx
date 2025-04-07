537a6e31c5e3aee359356ab4c4281b99010f78d7
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 16:20:33 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -24,7 +24,7 @@ export default function AboutUsPage() {
     </div>
 
     {/* Quote overlay */}
-    <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-10 text-center">
+    <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 z-10 text-center">
       <p className="text-white font-semibold text-[32px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
         Driven by Excellence, Energized by Trust.
       </p>
@@ -32,17 +32,26 @@ export default function AboutUsPage() {
   </div>
 
   {/* Main content */}
-  <div className="max-w-[1300px] px-6 md:px-[100px] flex flex-col mt-10">
+  <div className="border border-black max-w-[1300px] px-6 md:px-[100px] flex flex-col mt-10">
     <div className="text-black text-[24px] mb-4">Our journey</div>
     <div className="flex flex-row items-start gap-6">
       <div className="w-[10px] h-[50px] bg-[#FF0105]"></div>
       <div className="text-black text-[16px]">
-        {/* Long text here */}
+        Alver Power Systems is a company that was founded and registered in 2022 in accordance with the Kenya Company Act rules, however it has been in operation as a sole proprietor since 2017. 
+        Our goal is to offer cost-effective power solutions and sustainable, renewable energy-saving solutions that are both inexpensive and environmentally beneficial for consumers, businesses, and institutions.
+        In order to consistently produce high-quality electrical solutions that perform and provide protection, we at Alver Power System have formed our company around understanding the needs and expectations of our customers. 
+        Our work is done with no exceptions, and it shows in the final outcome. Every project is handled equally, whether it involves industrial buildings, residential structures, or commercial and retail fit-outs. 
+        Experienced project managers, licensed electricians, and office-based support staff guarantee the effective completion of your contract. 
+        Our consumers' faith and trust in using our services has contributed to our success. We are committed to delivering high-quality services on time and under budget, keeping everyone safe, 
+        and working together with all parties.
+        A focus on quality control and customer service means that a significant amount of our business originates from clients who frequently use our services.
       </div>
     </div>
   </div>
 
-  <Footer />
+  <div className='mt-[20px]'>
+    <Footer />
+  </div>
 </section>
 
   );
