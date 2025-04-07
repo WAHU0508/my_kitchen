585fd603c4932f2daf10b78a23b41a4a46419eb9
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 12:32:18 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -8,9 +8,9 @@ export default function AboutUsPage() {
           <p className='lg:text-[32px] text-semibold text-black lg:ml-[90px] md:w-[327px]  lg:w-[327px] w-[214px]'>About Us - Alver Power Systems.Ltd</p>
         </div> */}
       </div>
-      <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>
+{/*       <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>
         
-      </div>
+      </div> */}
     </section>
   )
 }
