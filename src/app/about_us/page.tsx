b9a771ceaf1e61a61e8b82d3a69c49c13b708c42
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 15:12:18 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import Image from 'next/image'
-import coverImg from '../assets/sivacon.jpg'
-import Header from '../components/header'
+import Image from 'next/image';
+import coverImg from '../assets/sivacon.jpg';
+import Header from '../components/header';
 
-export default function AboutUsPage() {  
+export default function AboutUsPage() {
   return (
-    <section className='relative top-0 w-full flex flex-col items-center justify-center'>
-      <div className='lg:w-[1440px] flex items-center justify-center'>
+    <section className='relative top-0 w-full flex flex-col items-center justify-center overflow-hidden'>
+
+      {/* Background Image */}
+      <div className='absolute top-0 left-0 w-[1440px] h-[300px] z-0'>
         <Image
           src={coverImg}
           alt='Cover Image'
-          width={1440}
-          height={300}
-          className='absolute'
+          fill
+          className='object-cover'
         />
       </div>
-      <Header />
+
+      {/* Header in front */}
+      <div className='relative z-10 w-full'>
+        <Header />
+      </div>
+      
     </section>
-  )
+  );
 }
