81fa95991053d484cd4d7aedbb0d224c066ef2b6
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-07 12:46:07 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -5,12 +5,19 @@ export default function AboutUsPage() {
     <section className='w-full flex items-center justify-center'>
       <div className='w-full md:h-[141px] lg:h-[141px] flex items-center justify-center bg-[#CACACA]'>
         <div className='w-full lg:w-[1300px]'>
-          <p className='lg:text-[32px] text-semibold text-black lg:ml-[90px] md:w-[327px]  lg:w-[327px] w-[214px]'>About Us - Alver Power Systems.Ltd</p>
+          <p className='lg:text-[32px] font-semibold text-black md:w-[327px] ml-[20px] lg:w-[327px] w-[214px]'>About Us - Alver Power Systems Ltd.</p>
         </div>
       </div>
-{/*       <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>
-        
-      </div> */}
+      <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>
+        <p className='w-full lg:w-[1186px] text-[#000000] text-[12px] lg:text-[24px] md:text-[20px]'>
+          Alver Power Systems is a company that was founded and registered in 2022 in accordance with the Kenya Company Act rules, 
+          however it has been in operation as a sole proprietor since 2017. Our goal is to offer cost-effective power solutions and sustainable, 
+          renewable energy-saving solutions that are both inexpensive and environmentally beneficial for consumers, businesses, and institutions.
+          In order to consistently produce high-quality electrical solutions that perform and provide protection, we at Alver Power System have formed our company around understanding 
+          the needs and expectations of our customers. Our work is done with no exceptions, and it shows in the final outcome. Every project is handled equally, whether it involves industrial buildings, residential structures, or commercial and retail fit-outs. Experienced project managers, licensed electricians, and office-based support staff guarantee the effective completion of your contract. Our consumers' faith and trust in using our services has contributed to our success. We are committed to delivering high-quality services on time and under budget, keeping everyone safe, and working together with all parties.
+          A focus on quality control and customer service means that a significant amount of our business originates from clients who frequently use our services.
+        </p>
+      </div>
     </section>
   )
 }
