15779b1f96e8678325201a6fb46be7fdad9c25ad
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-08 14:24:59 +0300

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
     return (
         <footer className='w-full mb-4 flex items-center justify-center bg-[#F8F8F8]'>
             {/* Footer for large and medium screens*/}
-            <div className='hidden lg:flex lg:flex-col text-black mt-[20px] md:flex md:flex-col items-center justify-between lg:px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#F8F8F8]'>
+            <div className='hidden lg:flex lg:flex-col text-black font-light mt-[20px] md:flex md:flex-col items-center justify-between lg:px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#F8F8F8]'>
                 <div className='flex flex-row justify-between gap-[10px] lg:gap-[50px]'>
                     <div className='lg:w-[321px] lg:h-[137px] w-[250px] h-[137px] flex flex-col items-center justify-between'>
                         <Clock size={31} className='text-black'/>
