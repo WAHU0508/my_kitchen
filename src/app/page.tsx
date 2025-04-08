57ac7aae66ee5ad805a1718edde218d4baaab3c8
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-08 15:47:11 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,36 +83,9 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-{/*       main section */}
-      <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>
-        {/*Image section*/}
-        <div className='hidden lg:flex lg:flex-row lg:gap-6 md:flex md:flex-row gap-2 justify-center'>
-          <Image 
-            src={solarpanelImg}
-            alt='Solar Panels Image'
-            width={250}
-            height={250}
-          />
-          <Image 
-            src={switchboardImg}
-            alt='Switchboard Image'
-            width={250}
-            height={250}
-          />
-          <Image 
-            src={electricalsImg}
-            alt='Electricals Image'
-            width={250}
-            height={250}
-          />
-          <Image 
-            src={avrImg}
-            alt='AVR Image'
-            width={250}
-            height={250}
-          />
-        </div>
-        
+      
+      {/* main section */}
+      <div className='lg:p-4 md:p-2 p-1 lg:w-[1300px] md:w-full w-full flex flex-col'>  
         {/*Small Screen*/}
         <div className='lg:hidden md:hidden flex items-center justify-center'>
           <Carousel />
@@ -121,7 +94,7 @@ export default function HomePage() {
         <h1 className='w-full flex lg:items-center lg:justify-center md:items-center md:justify-center text-[#FF0105] text-[16px] lg:text-[32px] md:text-[28px]'>Powering Innovation, Ensuring Reliability</h1>
         <p className='w-full lg:w-[1186px] flex lg:items-center lg:justify-center md:items-center md:justify-center text-center text-[#000000] text-[12px] lg:text-[24px] md:text-[20px]'>
           Alver Power Systems Ltd is your trusted partner for innovative electrical and renewable energy solutions across Kenya and East Africa.
-We specialize in high-quality electrical installations, solar energy solutions and custom switchboard manufacturing. We also offer end to end services that include engineering, procurement, construction (EPC) and maintenance.
+          We specialize in high-quality electrical installations, solar energy solutions and custom switchboard manufacturing. We also offer end to end services that include engineering, procurement, construction (EPC) and maintenance.
         </p>
         
         <div className='w-full flex flex-row items-center justify-center lg:gap-[200px] md:gap-[150px] gap-[50px] mt-[20px]'>
