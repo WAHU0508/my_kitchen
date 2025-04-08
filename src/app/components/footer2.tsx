956d565b6ee1331e9b790030d16fb2eb7fbb670f
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-08 12:43:48 +0300

    Update footer2.tsx

--- src/app/components/footer2.tsx
+++ src/app/components/footer2.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function () {
   return (
-    <div classname='w-full h-[300px] flex items-center justify-center bg-[#F8F8F8]'>
-      <div className='w-[1300px] clip-path-[polygon(0%_0%,_100%_0%,_50%_100%)]'></div>
+    <div className='w-full h-[300px] flex items-center justify-center bg-[#F8F8F8]'>
+      <div className='w-[1300px] h-[300px] clip-path-[polygon(0%_0%,_100%_0%,_50%_100%)]'></div>
     </div>
   )
 }
