04ecaa89c09d3b4c51c5fa220e60e644e58e4254
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-08 12:32:37 +0300

    Create footer2.tsx

---
+++ src/app/components/footer2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div classname='w-full flex items-center justify-center bg-[#F8F8F8]'>
+      <div className='w-[1300px] h-[600px]'></div>
+    </div>
+  )
+}
