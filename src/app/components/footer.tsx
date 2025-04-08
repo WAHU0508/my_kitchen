44b9cf0d4e7f1bff4f983543e1f13e50b5f8a8c2
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-08 12:57:17 +0300

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
             <div className='hidden lg:flex lg:flex-col text-white md:flex md:flex-col items-center justify-between lg:px-[60px] lg:w-[1440px] md:w-full lg:h-[215px] md:h-[215px] bg-[#F8F8F8]'>
                 <div className='flex flex-row justify-between gap-[10px] lg:gap-[50px]'>
                     <div className='lg:w-[321px] lg:h-[137px] w-[250px] h-[137px] flex flex-col items-center justify-between'>
-                        <Clock size={31} className='text-white'/>
+                        <Clock size={31} className='text-black'/>
                         <p className='text-[18px] text-center'>Monday - Friday</p>
                         <p className='text-[18px] text-center'>8 AM - 5PM (Saturday 8AM - 1PM)</p>
                     </div>
