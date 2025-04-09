742caad10225657ab4aa3d8b3136ebeb4c30827d
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-09 12:43:25 +0000

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -52,7 +52,7 @@ export default function AboutUsPage() {
           </div>
       
           {/* Quote overlay */}
-          <div className="absolute bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
+          <div className="absolute lg:bottom-50 md:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
             <p className="text-white font-semibold text-[32px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
               Driven by Excellence, Energized by Trust.
             </p>
