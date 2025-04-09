8c9d1dc4584d7329bd92465055f95a8fbd238e5e
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-09 13:21:34 +0000

    Create item1.tsx

---
+++ src/app/components/item1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Product1() {
+  return (
+    <div className='border border-black w-[200px] h-[400px]'>
+    </div>
+  )
+}
