5a1bcb07617ec8d2c2c63538ed06b3063bb9cb11
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-09 16:38:03 +0300

    Update item1.tsx

--- src/app/components/item1.tsx
+++ src/app/components/item1.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Product1() {
   return (
-    <div className='border border-black w-[200px] h-[400px]'>
+    <div className='border border-black w-[200px] h-[400px] lg:w-[250px] lg:h-[400px] md:w-[250px] md:h-[400px]'>
     </div>
   )
 }
