07a0073f0f9dc0737f5979cfc830a729d5fbb6d2
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 12:52:11 +0300

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -41,8 +41,8 @@ export default function Header() {
                 <div className='flex flex-row gap-6 h-full text-[15px] text-black items-center justify-center'>
                     <Link
                           href="/"
-                          className={`border border-black h-full cursor-pointer hover:text-[#252865] hover:underline ${
-                            pathname === '/' ? 'text-red-600 underline' : 'text-black'
+                          className={`border border-black h-full flex items-center justify-center font-semibold cursor-pointer hover:text-[#252865] hover:underline ${
+                            pathname === '/' ? 'text-[#FF0105]' : 'text-black'
                           }`}
                         >
                           Home
