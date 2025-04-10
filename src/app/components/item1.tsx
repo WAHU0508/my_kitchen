3bbeeebc2f45088faf3c28458cc84efff5c2ef3c
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 11:11:58 +0300

    Update item1.tsx

--- src/app/components/item1.tsx
+++ src/app/components/item1.tsx
@@ -4,17 +4,18 @@ import solarimg from '../assets/solar-homes.png'
 
 export default function Product1() {
   return (
-    <div className="relative w-[200px] h-[400px] md:w-[250px] md:h-[400px] lg:w-[250px] lg:h-[400px]">
+    <div className="relative w-[200px] h-[400px] md:w-[250px] md:h-[400px] lg:w-[350px] lg:h-[500px]">
       <Image 
         src={solarimg}
         alt="Solar Img"
-        width={250}
-        height={400}
+        width={350}
+        height={500}
         className="w-full h-full object-cover"
       />
       <div className="absolute top-0 left-0 w-full h-full bg-none hover:bg-black/60 flex flex-col">
-        <div className="ml-[50px] mt-[30px] mb-4 flex flex-col">
-          <p className="text-white text-lg font-medium">01</p>
+        <div className="ml-[50px] mt-[100px] mb-4 gap-2 flex flex-col">
+          <p className="text-white text-[32px] font-bold">01</p>
+          <p className="text-white text-[32px] font-bold">Solar Solutions</p>
           <div className="w-[50px] border-b-2 border-[#252865]" />
         </div>
       </div>
