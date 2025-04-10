96e024cd19101290c92567b66d61af004d1a89b4
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 09:29:00 +0300

    Update item1.tsx

--- src/app/components/item1.tsx
+++ src/app/components/item1.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import solarimg from '../assets/sp.jpg'
+import solarimg from '../assets/sp.png'
 
 export default function Product1() {
   return (
