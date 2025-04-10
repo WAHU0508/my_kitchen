92e36a86eed4945e1108b1062d05823c8b7affa0
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 11:16:44 +0300

    Update item1.tsx

--- src/app/components/item1.tsx
+++ src/app/components/item1.tsx
@@ -12,10 +12,10 @@ export default function Product1() {
         className="object-cover"
       />
       <div className="absolute top-0 left-0 w-full h-full bg-none hover:bg-black/60 flex flex-col">
-        <div className="ml-[50px] mt-[100px] mb-4 gap-2 flex flex-col">
+        <div className="ml-[50px] mt-[100px] mb-4 flex flex-col">
           <p className="text-white text-[32px] font-bold">01</p>
           <p className="text-white text-[32px] font-bold">Solar Solutions</p>
-          <div className="w-[50px] border-b-2 border-[#252865]" />
+          <div className="w-[50px] border-b-2 border-[#252865]"></div></div>
         </div>
       </div>
     </div>
