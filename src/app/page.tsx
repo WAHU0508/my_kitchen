90a1f0e40667c178d69913e8d10691fd1572e4f9
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 15:00:38 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ export default function HomePage() {
                 {/*Small Screen*/}
                 <div className='relative w-full lg:hidden md:hidden flex flex-col items-center justify-center'>
                     <Carousel2 />
-                    <div className="fixed top-2 left-0 w-full lg:z-20">
+                    <div className="absolute top-2 left-0 w-full lg:z-20">
                         <Header />
                     </div>
                     <div className="absolute bottom-10 md:bottom-50 lg:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
