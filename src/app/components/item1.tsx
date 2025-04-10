9d7b5ee43fc2f29cd898106cbdb88bf138d88888
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 10:41:01 +0300

    Update item1.tsx

--- src/app/components/item1.tsx
+++ src/app/components/item1.tsx
@@ -12,7 +12,7 @@ export default function Product1() {
         height={400}
         className="w-full h-full object-cover"
       />
-      <div className="absolute w-full h-full bg-none hover:bg-black/60 flex flex-col">
+      <div className="absolute top-0 left-0 w-full h-full bg-none hover:bg-black/60 flex flex-col">
         <div className="ml-[50px] mb-4 flex flex-col">
           <p className="text-white text-lg font-medium">01</p>
           <div className="w-[50px] border-b-2 border-[#252865]" />
