c8277e74f76cab3d9426e3516f44d5da93f6b4f1
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-10 09:22:03 +0300

    Create itesm3.tsx

---
+++ src/app/components/itesm3.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import solarimg from '../assets/sp.jpg'
+
+export default function Product1() {
+  return (
+    <div className='border border-black w-[200px] h-[400px] lg:w-[250px] lg:h-[400px] md:w-[250px] md:h-[400px]'>
+      <Image 
+        src={solarimg}
+        alt='Solar Img'
+        width={250}
+        height={400}
+        className='w-full h-full object-cover'
+      />
+    </div>
+  )
+}
