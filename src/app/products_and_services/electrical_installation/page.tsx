86021b31173c12621e00497b4c15a4718e9f4217
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-11 16:17:19 +0300

    Create page.tsx

---
+++ src/app/products_and_services/electrical_installation/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation';
+
+export default function SolarSolutions() {
+    const router = useRouter();
+  
+    return (
+        <section className="w-full flex flex-col items-center justify-center">
+            <div className='relative w-full lg:w-[1440px] flex flex-col items-center justify-center overflow-x-hidden overflow-y-auto'>
+                {/* Background Image with header and quote overlay */}
+                <div className="relative w-full h-[300px] lg:w-[1440px] lg:h-[550px] md:h-[550px]">
+                    <Image
+                        src='/img03.jpg'
+                        alt="Cover Image"
+                        fill
+                        className="object-cover"
+                        priority
+                    />
+                    <div className="absolute bottom-20 lg:bottom-50 md:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
+                        <p className="text-white font-semibold text-[32px] lg:text-[50px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
+                            CNC
+                        </p>
+                    </div>
+                </div>
+
+                {/* Main section */}
+          
+
+            </div>
+        </section>
+    )
+}
