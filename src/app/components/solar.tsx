908b20fd45db7e497b10e5b9b5f53d791c9072a6
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-11 12:01:13 +0300

    Create solar.tsx

---
+++ src/app/components/solar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import solarimg from '../assets/img01.jpg'
+
+export default function Product1() {
+  return (
+    <div className="relative w-[320px] h-[200px] group overflow-hidden">
+      <Image 
+        src={solarimg}
+        alt="Solar Img"
+        fill
+        className="object-cover"
+      />
+
+      {/* Hover overlay */}
+      <div className="absolute top-0 left-0 w-full h-full bg-transparent group-hover:bg-black/60 transition-colors duration-300 flex flex-col">
+        <div className="ml-[50px] mt-[100px] mb-4 flex flex-col gap-3">
+          <p className="text-white text-[32px] font-bold">01</p>
+          <p className="text-white text-[32px] font-bold">Solar Solutions</p>
+          <div className="w-[50px] group-hover:w-[150px] border-b-4 border-[#FF0105] transition-all duration-300"></div>
+          <button className='px-1 py-2 border border-white text-[16px] text-white active:bg-white active:text-black opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-in-out'>
+            See More
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
