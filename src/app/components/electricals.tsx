00461c37aa8ec2b34db75ace425482296ce4570d
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-11 12:42:55 +0300

    Update electricals.tsx

--- src/app/components/electricals.tsx
+++ src/app/components/electricals.tsx
@@ -1,13 +1,13 @@
 
 import React from 'react'
 import Image from 'next/image'
-import sivaconimg from '../assets/sivacon.jpg'
+import electricalsimg from '../assets/img03.jpg'
 
 export default function Product1() {
   return (
     <div className="relative w-[320px] h-[250px] group overflow-hidden">
       <Image 
-        src={sivaconimg}
+        src={electricalsimg}
         alt="Solar Img"
         fill
         className="object-cover"
@@ -16,8 +16,8 @@ export default function Product1() {
       {/* Hover overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-transparent group-hover:bg-black/60 transition-colors duration-300 flex flex-col">
         <div className="ml-[20px] mt-[10px] mb-4 flex flex-col gap-3">
-          <p className="text-white text-[32px] font-bold">01</p>
-          <p className="text-white text-[32px] font-bold">Switchboard Manufacturing</p>
+          <p className="text-white text-[32px] font-bold">04</p>
+          <p className="text-white text-[32px] font-bold">Electrical Installation</p>
           <div className="w-[50px] group-hover:w-[150px] border-b-4 border-[#FF0105] transition-all duration-300"></div>
           <button className='w-[120px] py-2 rounded-[5px] border border-white text-[16px] text-white active:bg-white active:text-black opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-in-out'>
             See More
