a08f617eb394b104c2c3358cc4fb4e11dfb54a04
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-11 16:23:28 +0300

    Update page.tsx

--- src/app/products_and_services/switchboard_manufacturing/page.tsx
+++ src/app/products_and_services/switchboard_manufacturing/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 export default function SolarSolutions() {
-    const router = useRouter();
+    // const router = useRouter();
   
     return (
         <section className="w-full flex flex-col items-center justify-center">
