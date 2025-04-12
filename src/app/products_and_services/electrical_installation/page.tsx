f4625657bd6c7cca330c75eef1e6d80a2e5fec2f
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-12 10:32:48 +0300

    Update page.tsx

--- src/app/products_and_services/electrical_installation/page.tsx
+++ src/app/products_and_services/electrical_installation/page.tsx
@@ -28,7 +28,7 @@ export default function SolarSolutions() {
                     </div>
                     <div className="absolute bottom-20 lg:bottom-50 md:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
                         <p className="text-white font-semibold text-[32px] lg:text-[50px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
-                            CNC
+                            Electrical Installation
                         </p>
                     </div>
                 </div>
