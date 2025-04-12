49d4231a72a13ba0bde84d8c81ec596362776786
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-12 10:12:42 +0300

    Update page.tsx

--- src/app/products_and_services/page.tsx
+++ src/app/products_and_services/page.tsx
@@ -30,7 +30,7 @@ export default function ProductsPage() {
             </div>
             {/* Heading */}
             <div className="absolute bottom-20 md:bottom-50 lg:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
-                <p className="text-white font-semibold text-[32px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
+                <p className="text-white font-semibold text-[24px] md:text-[30px] lg:text-[32px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
                     Our Range of Electrical & Engineering Solutions
                 </p>
             </div>
