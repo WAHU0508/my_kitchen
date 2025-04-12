5657c4285d4a4b48a9709c1faad2df5e16f6f6f3
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-12 08:20:24 +0300

    Update page.tsx

--- src/app/products_and_services/page.tsx
+++ src/app/products_and_services/page.tsx
@@ -32,12 +32,18 @@ export default function ProductsPage() {
           
           <div className="absolute bottom-5 lg:bottom-50 md:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
             <p className="text-white font-semibold text-[32px] lg:text-[50px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
-              Products and Services
+              Our Range of Electrical & Engineering Solutions
             </p>
           </div>
         </div>
         {/* Main section */}
-        <div className="w-full mt-[20px] overflow-x-auto lg:overflow-x-hidden lg:overflow-y-hidden">
+        <div className="w-full flex flex-col items-center justify-center mt-[20px] overflow-x-auto lg:overflow-x-hidden lg:overflow-y-hidden">
+            <h2 className="text-[#252865] text-3xl lg:text-5xl font-bold mb-6">
+              Our Products & Services
+            </h2>
+            <p className="text-gray-600 text-base lg:text-lg mb-10 max-w-3xl mx-auto">
+              From solar energy to precision CNC machining, we provide a full suite of expert solutions tailored to power industries, infrastructures, and innovations.
+            </p>
             <div className="flex flex-row items-center justify-center  min-w-max">
                 <Item1 />
                 <Item2 />
