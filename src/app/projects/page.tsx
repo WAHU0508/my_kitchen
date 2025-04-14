b672ec8d6090d68ec282337fcee2c348b7bb0b8e
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-14 11:44:33 +0300

    Add project cards

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import React from 'react'
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import coverImg from '../assets/portfolio.jpg';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import ProjectCard1 from './components/project_card1'
+import ProjectCard2 from './components/project_card2'
+import ProjectCard3 from './components/project_card3'
 
 export default function ProjectsPage() {
   return (
@@ -25,12 +29,27 @@ export default function ProjectsPage() {
       
           {/* Quote overlay */}
           <div className="absolute bottom-5 lg:bottom-50 md:bottom-50 left-1/2 transform -translate-x-1/2 z-10 text-center">
-            <p className="text-white font-semibold text-[32px] lg:text-[50px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
+            <p className="text-black font-semibold text-[32px] lg:text-[50px] md:text-[50px] w-[80vw] max-w-[800px] mx-auto">
               OUR projects
             </p>
           </div>
         </div>
       </div>
+      <div className="w-full overflow-x-auto lg:overflow-y-hidden flex items-center justify-center">
+          <motion.div
+            className="w-fit flex flex-col items-center justify-center gap-4 
+                       lg:grid lg:grid-cols-3 lg:gap-6 
+                       px-4 py-6"
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: 'easeOut' }}
+            viewport={{ once: false, amount: 0.2 }}
+          >
+            <ProjectCard1 />
+            <ProjectCard2 />
+            <ProjectCard3 />
+          </motion.div>
+        </div>
         <div className='w-full bg-[#252865] mt-[20px]'>
               <Footer />
         </div>
