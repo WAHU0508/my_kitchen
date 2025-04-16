88ea609ba0d243caf336998935fe2cc37c98133a
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-16 09:28:45 +0300

    Update page.tsx

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -255,7 +255,7 @@ export default function AboutUsPage() {
           </div>
         
       </div>
-      <div className='w-full bg-[#252865] mt-[20px]'>
+      <div className='w-full bg-[#F8F8F8] mt-[20px]'>
           <Footer />
       </div>
     </section>
