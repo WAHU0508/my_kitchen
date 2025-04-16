32a317fcb064718ec12b249b64fdb99253da0d2f
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-04-16 09:29:29 +0300

    Update page.tsx

--- src/app/products_and_services/page.tsx
+++ src/app/products_and_services/page.tsx
@@ -55,7 +55,7 @@ export default function ProductsPage() {
             </div>
         </div>
         {/* Footer Section */}
-        <div className='w-full bg-[#252865] mt-[20px]'>
+        <div className='w-full bg-[#F8F8F8] mt-[20px]'>
             <Footer />
         </div>
     </div>
