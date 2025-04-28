3119ab46e22b4fdb4c9c5cf3b54adfa4002625aa
WAHU0508 <sharonwg0508@gmail.com>
2025-04-28 13:46:41 +0300

    Overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -435,7 +435,7 @@ export default function HomePage() {
                         <p className='text-[#FF0105]'>Our</p>
                         <p className='text-[#252865]'>Projects</p>
                     </motion.div>
-                    <div className="w-full overflow-x-hidden lg:overflow-y-hidden">
+                    <div className="w-full overflow-x-auto lg:overflow-x-hidden lg:overflow-y-hidden">
                       <motion.div
                         className="w-full flex flex-row items-center justify-center gap-2 lg:gap-4 xl:gap-4 2xl:gap-[100px] min-w-max"
                         initial={{ opacity: 0, y: 40 }}
