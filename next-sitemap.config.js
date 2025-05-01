46eb6ace2a7f53a2ffebb74f2372c19dcf747966
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-05-01 06:54:35 +0300

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://alverpower.com', // Replace with your domain
+  generateRobotsTxt: true, // Will also generate robots.txt
+};
