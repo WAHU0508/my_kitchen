25967216a4250293db907f6dba3dde1f9b380f56
WAHU0508 <sharonwg0508@gmail.com>
2025-05-06 16:18:14 +0300

    Edit header styling in medium and small screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,12 +21,12 @@ export default function Header() {
     <header className="w-full flex items-center justify-center">
       {/* Header for large and medium screens */}
       <div
-        className={`hidden w-full transition-all duration-500 ease-in-out md:flex md:flex-row items-center justify-between text-white px-8 py-2 ${
+        className={`hidden w-full transition-all duration-500 ease-in-out md:flex md:flex-row items-center justify-between text-white lg:px-8 py-2 ${
           isScrolled ? 'bg-white' : 'bg-black/60'
         } transition-all duration-300 sticky top-0 z-50`}
       >
         <Image src={logo} alt="Alver Power Logo" width={150} height={45} />
-        <nav className="flex flex-row gap-6 items-center">
+        <nav className="flex flex-row md:gap-2 lg:gap-6 items-center">
           <p
             className={`${
               isScrolled ? 'text-black' : 'text-white'
@@ -63,7 +63,7 @@ export default function Header() {
 
       {/* header for smaller screens */}
       <div className='lg:hidden md:hidden flex flex-row w-full items-center gap-2 mt-2 ml-2 mr-[20px]'>
-        <div className='border border-[#0000003D] bg-[#FFFFFF] flex items-center justify-center shadow-xl rounded-[10px] w-full h-[40px]'>
+        <div className='bg-black/60 flex items-center justify-center shadow-xl rounded-[10px] w-full'>
             <Image
                 src={logo}
                 width={120}
