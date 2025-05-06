f19a18196254c2dfea97b59d3379f9a3d52bb4b5
WAHU0508 <sharonwg0508@gmail.com>
2025-05-06 14:29:14 +0300

    Edit menu styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,24 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image'
 import logo from '@//assets/logo.png'
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+      const handleScroll = () => {
+        setIsScrolled(window.scrollY > 50)
+      }
+
+      window.addEventListener('scroll', handleScroll)
+      return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
 
     return (
         <header className='w-full flex items-center justify-center'>
             {/* Header for large and medium screens */}
-            <div className='hidden w-full md:flex md:flex-row items-center justify-between bg-black/60 text-white px-4 py-2'>
+            <div className={`hidden w-full md:flex md:flex-row items-center justify-between text-white px-4 py-2 ${isScrolled? 'bg-white' : 'bg-black/60' }`}>
               <Image
                 src={logo}
                 alt='Alver Power Logo'
@@ -16,10 +26,11 @@ export default function Header() {
                 height={45}
               />
               <nav className='flex flex-row gap-6 items-center'>
-                <p>Home</p>
-                <p>About</p>
-                <p>Products & Services</p>
-                <p>Contact Us</p>
+                <p className={`${isScrolled? 'text-black' : 'text-white'} text-[18px] hover:text-[#C45308] cursor-pointer`}>Home</p>
+                <p className={`${isScrolled? 'text-black' : 'text-white'} text-[18px] hover:text-[#C45308] cursor-pointer`}>About</p>
+                <p className={`${isScrolled? 'text-black' : 'text-white'} text-[18px] hover:text-[#C45308] cursor-pointer`}>Products & Services</p>
+                <p className={`${isScrolled? 'text-black' : 'text-white'} text-[18px] hover:text-[#C45308] cursor-pointer`}>Contact Us</p>
+                <button className='bg-black text-white px-2 py-1 hover:bg-white hover:text-black'>Get Quote</button>
               </nav>
             </div>
         </header>
