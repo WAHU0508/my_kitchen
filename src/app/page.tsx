7f10b1b2084b6071ed77e42f210ec4db19564c47
WAHU0508 <sharonwg0508@gmail.com>
2025-05-06 15:37:24 +0300

    Edit positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function HomePage() {
     return (
         <div className='text-black relative'>
             <div className='w-full relative'>
-                <Header className='fixed top-0 left-0 z-20'/>
+                <div className='fixed top-0 left-0 z-20'>
+                    <Header/>
+                </div>
                 <video
                     src='/Myvideo.mp4'
                     autoPlay
