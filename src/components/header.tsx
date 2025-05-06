45c96c7fc847936323f4f210d6f6d7ce10cc0fa2
WAHU0508 <sharonwg0508@gmail.com>
2025-05-06 13:02:36 +0300

    Edit header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className='w-full flex items-center justify-center'>
             {/* Header for large and medium screens */}
-            <div className='text-black'>Header</div>
+            <div className='hidden w-full md:flex md:flex-row bg-black/60 text-black px-4 py-2'>Header</div>
         </header>
     );
 }
