9367174204709c6413a13dbb5c33302b333e53b5
WAHU0508 <sharonwg0508@gmail.com>
2025-05-07 19:47:05 +0300

    Add Width

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 
 function MissionSection() {
   return (
-    <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[70px] xl:pt-[30px]'>
-        <div className='w-[80%] border-l-1 border-white h-auto flex lg:flex-row xl:flex-row'>
-            <div className='mt-[50px] border-l-2 border-white lg:h-[100px] xl:h-[100px]'></div>
-            <div className='flex flex-col lg:ml-[50px] xl:ml-[50px]'>
-                <p className='lg:text-[14px] xl:text-[14px]'>Mission</p>
-                <p className='lg:text-[18px] lg:mt-[50px] xl:text-[18px] xl:mt-[50px]'>Our mission is to build lasting, mutually beneficial relationships with each of our esteemed 
+    <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[70px]'>
+        <div className='w-[80%] border-l-1 border-white h-auto flex lg:flex-row'>
+            <div className='mt-[50px] border-l-2 border-white lg:h-[100px]'></div>
+            <div className='flex flex-col lg:ml-[50px] lg:w-[300px]'>
+                <p className='lg:text-[14px]'>Mission</p>
+                <p className='lg:text-[18px] lg:mt-[50px]'>Our mission is to build lasting, mutually beneficial relationships with each of our esteemed 
                     clients by continually exceeding their expectations and earning their confidence via the 
                     excellent and devoted work of every member of the Alver power System Team. Also to offer 
                     quality, safe, innovative, and flexible electrical services by ensuring commitment to the 
