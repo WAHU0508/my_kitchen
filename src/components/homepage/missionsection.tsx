7b73692024f07603c6003b6e6448b20390fa2e20
WAHU0508 <sharonwg0508@gmail.com>
2025-05-07 19:10:06 +0300

    Edit

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -3,11 +3,16 @@ import React from 'react'
 function MissionSection() {
   return (
     <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[30px]'>
-        <div className='w-[80%] border-l-2 border-white h-auto flex lg:flex-row'>
+        <div className='w-[80%] border-l-1 border-white h-auto flex lg:flex-row'>
             <div className='mt-[50px] border-l-2 border-white lg:h-[100px]'></div>
             <div className='flex flex-col lg:ml-[50px]'>
-                <p className='lg:text-[24px]'>Mission Statement</p>
-                <p className='lg:text-[18px] lg:mt-[50px]'>Mission words</p>
+                <p className='lg:text-[14px]'>Mission</p>
+                <p className='lg:text-[18px] lg:mt-[50px]'>Our mission is to build lasting, mutually beneficial relationships with each of our esteemed 
+                    clients by continually exceeding their expectations and earning their confidence via the 
+                    excellent and devoted work of every member of the Alver power System Team. Also to offer 
+                    quality, safe, innovative, and flexible electrical services by ensuring commitment to the 
+                    implementation or supporting technical, managerial and business operational systems
+                </p>
             </div>
         </div>
     </div>
