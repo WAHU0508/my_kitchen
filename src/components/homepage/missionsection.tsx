66ebe2890481374e4d63f20a30eebd6c1d4ca9dc
WAHU0508 <sharonwg0508@gmail.com>
2025-05-07 16:26:40 +0300

    Edit

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -4,10 +4,10 @@ function MissionSection() {
   return (
     <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[30px]'>
         <div className='w-[80%] border-l-2 border-white h-auto flex lg:flex-row'>
-            <div className='mt-[50px] border-l-2 border-white h-auto flex lg:flex-row'></div>
-            <div className='flex flex-col lg:mr-[50px]'>
-             <p className='lg:text-[24px]'>Mission Statement</p>
-             <p className='lg:text-[18px] lg:mt-[50px]'>Mission words</p>
+            <div className='mt-[50px] border-l-2 border-white lg:h-[100px]'></div>
+            <div className='flex flex-col lg:ml-[50px]'>
+                <p className='lg:text-[24px]'>Mission Statement</p>
+                <p className='lg:text-[18px] lg:mt-[50px]'>Mission words</p>
             </div>
         </div>
     </div>
