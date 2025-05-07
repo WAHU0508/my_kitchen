dbd7a330e1da27fb703c67c7c55b010e5f2147ca
WAHU0508 <sharonwg0508@gmail.com>
2025-05-07 19:48:55 +0300

    Add Width

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -7,7 +7,7 @@ function MissionSection() {
     <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[70px]'>
         <div className='w-[80%] border-l-1 border-white h-auto flex lg:flex-row'>
             <div className='mt-[50px] border-l-2 border-white lg:h-[100px]'></div>
-            <div className='flex flex-col lg:ml-[50px] lg:w-[300px]'>
+            <div className='flex flex-col lg:ml-[50px] lg:w-[600px]'>
                 <p className='lg:text-[14px]'>Mission</p>
                 <p className='lg:text-[18px] lg:mt-[50px]'>Our mission is to build lasting, mutually beneficial relationships with each of our esteemed 
                     clients by continually exceeding their expectations and earning their confidence via the 
