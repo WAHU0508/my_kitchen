a5a2b2333978db1836907432cbbe21cf009a89c4
WAHU0508 <sharonwg0508@gmail.com>
2025-05-07 16:24:46 +0300

    Edit mission

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -5,7 +5,10 @@ function MissionSection() {
     <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[30px]'>
         <div className='w-[80%] border-l-2 border-white h-auto flex lg:flex-row'>
             <div className='mt-[50px] border-l-2 border-white h-auto flex lg:flex-row'></div>
-            <p>Mission Statement</p>
+            <div className='flex flex-col lg:mr-[50px]'>
+             <p className='lg:text-[24px]'>Mission Statement</p>
+             <p className='lg:text-[18px] lg:mt-[50px]'>Mission words</p>
+            </div>
         </div>
     </div>
   )
