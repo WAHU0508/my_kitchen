a619a3146f3156814b534c7e724a327edb0207b8
WAHU0508 <sharonwg0508@gmail.com>
2025-05-07 16:19:40 +0300

    Add borders

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function MissionSection() {
   return (
-    <div className='w-full bg-black h-auto text-white'> Mission Section</div>
+    <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[30px]'>
+        <div className='w-[80%] border-l-2 border-white h-auto flex lg:flex-row'>
+            <div className='border-l-2 border-white h-auto flex lg:flex-row'></div>
+            <p>Mission Statement</p>
+        </div>
+    </div>
   )
 }
 
