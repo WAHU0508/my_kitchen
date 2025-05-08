9ac24b3676f310b89eb590d3773ff05d9f7b8928
WAHU0508 <sharonwg0508@gmail.com>
2025-05-08 11:22:16 +0300

    Add expertise section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,22 @@ export default function HomePage() {
             <div className='w-full mt-[10px] md:mt-[50px] lg:mt-[130px]'>
                 <MissionSection/>
             </div>
+
+            {/* Our Expertise */}
+            <div className='w-full bg-white h-auto text-white flex items-center justify-center lg:pt-[70px]'>
+                <div className=' w-[95%] md:w-[95%] lg:w-[80%] border-l-1 border-black h-auto flex flex-row'>
+                    <div className='mt-[50px] border-l-2 border-black h-[100px] md:h-[100px] lg:h-[100px]'></div>
+                    <div className='w-full flex flex-col md:flex-row lg:flex-row'>
+                        <div className='flex flex-col ml-[30px] md:ml-[30px] w-[200px] md:w-[500px] lg:ml-[50px] lg:w-[500px]'>
+                            <p className='text-[12px] md:text-[14px] lg:text-[14px]'>Our Expertise</p>
+                            <p className='text-[18px] md:text-[20px] lg:text-[24px] mt-[30px] md:mt-[30px] lg:mt-[50px]'>
+                                Delivering Excellence Across Diverse Services
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
