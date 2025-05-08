717f176e9242cca8513379e6a287d04b4274e431
WAHU0508 <sharonwg0508@gmail.com>
2025-05-08 10:56:19 +0300

    Styling smaller screens

--- src/components/homepage/missionsection.tsx
+++ src/components/homepage/missionsection.tsx
@@ -5,22 +5,24 @@ import Image from 'next/image';
 function MissionSection() {
   return (
     <div className='w-full bg-black h-auto text-white flex items-center justify-center lg:pt-[70px]'>
-        <div className=' w-[95%] md:w-[95%] lg:w-[80%] border-l-1 border-white h-auto flex flex-col md:flex-row lg:flex-row'>
-            <div className='mt-[50px] border-l-2 border-white h-[30px] md:h-[100px] lg:h-[100px]'></div>
-            <div className='flex flex-col ml-[30px] md:ml-[30px] md:w-[600px] lg:ml-[50px] lg:w-[600px]'>
-                <p className='text-[12px] md:text-[14px] lg:text-[14px]'>Mission</p>
-                <p className='text-[16px] md:text-[18px] lg:text-[18px] mt-[30px] md:mt-[30px] lg:mt-[50px]'>Our mission is to build lasting, mutually beneficial relationships with each of our esteemed 
-                    clients by continually exceeding their expectations and earning their confidence via the 
-                    excellent and devoted work of every member of the Alver power System Team. Also to offer 
-                    quality, safe, innovative, and flexible electrical services by ensuring commitment to the 
-                    implementation or supporting technical, managerial and business operational systems
-                </p>
+        <div className=' w-[95%] md:w-[95%] lg:w-[80%] border-l-1 border-white h-auto flex flex-row'>
+            <div className='mt-[50px] border-l-2 border-white h-[100px] md:h-[100px] lg:h-[100px]'></div>
+            <div className='flex flex-col md:flex-row lg:flex-row'>
+                <div className='flex flex-col ml-[30px] md:ml-[30px] md:w-[600px] lg:ml-[50px] lg:w-[600px]'>
+                    <p className='text-[12px] md:text-[14px] lg:text-[14px]'>Mission</p>
+                    <p className='text-[16px] md:text-[18px] lg:text-[18px] mt-[30px] md:mt-[30px] lg:mt-[50px]'>Our mission is to build lasting, mutually beneficial relationships with each of our esteemed 
+                        clients by continually exceeding their expectations and earning their confidence via the 
+                        excellent and devoted work of every member of the Alver power System Team. Also to offer 
+                        quality, safe, innovative, and flexible electrical services by ensuring commitment to the 
+                        implementation or supporting technical, managerial and business operational systems
+                    </p>
+                </div>
+                <Image
+                    src={sivacon}
+                    alt='Sivacon Image'
+                    className='w-full h-auto object-contain'
+                />
             </div>
-            <Image
-                src={sivacon}
-                alt='Sivacon Image'
-                className='w-full h-auto object-contain'
-            />
         </div>
     </div>
   )
