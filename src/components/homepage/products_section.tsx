90ba0aaf5c5f39058d605c5984b07b062ea67fc6
WAHU0508 <sharonwg0508@gmail.com>
2025-05-09 16:52:30 +0300

    Edit switchboard padding btn

--- src/components/homepage/products_section.tsx
+++ src/components/homepage/products_section.tsx
@@ -4,7 +4,7 @@ import sivacon from '@//assets/sivacon(2).jpg'
 
 function ProductsSection() {
   return (
-    <div className='w-full bg-white h-auto text-black flex flex-col md:flex-row items-center justify-center'>
+    <div className='w-full bg-white h-auto text-black flex flex-col md:flex-row items-center justify-center mt-[20px]'>
         <div className='w-[95%] md:w-[95%] lg:w-[80%] h-auto flex flex-col md:flex-row items-center justify-center'>
             <div className='w-[50%]'>
                 <div className='text-black lg:w-[70%] flex flex-col gap-[50px]'>
@@ -13,8 +13,8 @@ function ProductsSection() {
                         we offer high-quality manufacturing solutions tailored to meet your specific requirements.
                     </p>
                     <button className='bg-white text-black flex flex-row'>
-                        <p className='border border-black px-2 py-1'>read more</p>
-                        <p className='border-l border black px-2 py-1'>A</p>
+                        <p className='border border-black px-4 py-1'>read more</p>
+                        <p className='border border-l-0 border-black px-4 py-1'>A</p>
                     </button>
                 </div>
                 
