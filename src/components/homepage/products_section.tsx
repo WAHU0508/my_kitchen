80b2ff6b3ed4fc5df96777767f2fe5a4365b8100
WAHU0508 <sharonwg0508@gmail.com>
2025-05-09 16:38:48 +0300

    Edit switchboard text

--- src/components/homepage/products_section.tsx
+++ src/components/homepage/products_section.tsx
@@ -7,12 +7,12 @@ function ProductsSection() {
     <div className='w-full bg-white h-auto text-black flex flex-col md:flex-row items-center justify-center'>
         <div className='w-[95%] md:w-[95%] lg:w-[80%] h-auto flex flex-col md:flex-row items-center justify-center'>
             <div className='w-[50%]'>
-                <div className='text-black lg:w-[80%] flex flex-col'>
+                <div className='text-black lg:w-[80%] flex flex-col gap-4'>
                     <p className='lg:text-[40px]'>Switchboard Manufacturing</p>
                     <p className='lg:text-[16px]'>From custom-designed switch boards to panel assemblies, 
                         we offer high-quality manufacturing solutions tailored to meet your specific requirements.
                     </p>
-                    <button className='border border-black bg-white text-black px-2 py-1'>
+                    <button className='border border-black bg-white text-black px-2 py-1 flex flex-row'>
                         <p>read more</p>
                         <p className='border-l border black'>A</p>
                     </button>
