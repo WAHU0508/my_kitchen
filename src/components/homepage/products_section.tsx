e4abad8be99abcda686dcac043723390b1b6872c
WAHU0508 <sharonwg0508@gmail.com>
2025-05-10 09:55:28 +0300

    Edit medium screen product

--- src/components/homepage/products_section.tsx
+++ src/components/homepage/products_section.tsx
@@ -6,8 +6,8 @@ function ProductsSection() {
   return (
     <div className='w-full bg-white h-auto text-black flex flex-col md:flex-row items-center justify-center pt-[20px]'>
         <div className='w-full h-auto flex flex-col md:flex-row items-center justify-center md:justify-between md:pl-[5%] lg:pl-[10%]'>
-            <div className='w-[95%] md:w-[50%]'>
-                <div className='text-black w-full flex flex-col gap-[20px] md:gap-[50px] lg:gap-[70px]'>
+            <div className='w-[95%] md:w-[50%] h-auto'>
+                <div className='text-black w-full flex flex-col items-center justify-between'>
                     <p className='text-[24px] md:text-[32px] lg:text-[40px]'>Switchboard Manufacturing</p>
                     <p className='text-[12px] md:text-[16px] lg:text-[16px]'>From custom-designed switch boards to panel assemblies, 
                         we offer high-quality manufacturing solutions tailored to meet your specific requirements.
