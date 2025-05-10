57f722fd33100af32c732903c53a84a9295ad22b
WAHU0508 <sharonwg0508@gmail.com>
2025-05-10 08:31:30 +0300

    Add padding

--- src/components/homepage/products_section.tsx
+++ src/components/homepage/products_section.tsx
@@ -5,7 +5,7 @@ import sivacon from '@//assets/sivacon(2).jpg'
 function ProductsSection() {
   return (
     <div className='w-full bg-white h-auto text-black flex flex-col md:flex-row items-center justify-center pt-[20px]'>
-        <div className='w-[95%] md:w-[95%] lg:w-[80%] h-auto flex flex-col md:flex-row items-center justify-center'>
+        <div className='w-full h-auto flex flex-col md:flex-row items-center justify-center pl-[100px]'>
             <div className='w-[50%]'>
                 <div className='text-black lg:w-[70%] flex flex-col gap-[50px]'>
                     <p className='lg:text-[40px]'>Switchboard Manufacturing</p>
@@ -17,7 +17,6 @@ function ProductsSection() {
                         <p className='border border-l-0 border-black px-4 py-1 rounded-tr-[10px] rounded-br-[10px]'>A</p>
                     </button>
                 </div>
-                
             </div>
             <div className='w-[50%] h-auto'>
                 <Image
