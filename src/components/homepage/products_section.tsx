49956d78569ff4310f55defc8686b33b601f859b
WAHU0508 <sharonwg0508@gmail.com>
2025-05-10 09:25:38 +0300

    Edit small screen product

--- src/components/homepage/products_section.tsx
+++ src/components/homepage/products_section.tsx
@@ -5,14 +5,14 @@ import sivacon from '@//assets/sivacon(2).jpg'
 function ProductsSection() {
   return (
     <div className='w-full bg-white h-auto text-black flex flex-col md:flex-row items-center justify-center pt-[20px]'>
-        <div className='w-full h-auto flex flex-col md:flex-row items-center justify-center md:justify-between lg:pl-[10%]'>
+        <div className='w-full h-auto flex flex-col md:flex-row items-center justify-center md:justify-between md:pl-[5%] lg:pl-[10%]'>
             <div className='w-[95%] md:w-[50%]'>
                 <div className='text-black w-full md:w-[50%] lg:w-[70%] flex flex-col gap-[20px] md:gap-[50px]'>
                     <p className='text-[24px] md:text-[32px] lg:text-[40px]'>Switchboard Manufacturing</p>
                     <p className='text-[12px] md:text-[14px] lg:text-[16px]'>From custom-designed switch boards to panel assemblies, 
                         we offer high-quality manufacturing solutions tailored to meet your specific requirements.
                     </p>
-                    <button className='bg-white text-black flex flex-row'>
+                    <button className='bg-white text-black flex flex-row mb-[20px] md:mb-0'>
                         <p className='border border-black px-4 py-1 rounded-tl-[10px] rounded-bl-[10px]'>read more</p>
                         <p className='border border-l-0 border-black px-4 py-1 rounded-tr-[10px] rounded-br-[10px]'>A</p>
                     </button>
