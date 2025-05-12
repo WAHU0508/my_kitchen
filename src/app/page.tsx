8c1b059bc66338554c9d25ba922db969df731dd2
WAHU0508 <sharonwg0508@gmail.com>
2025-05-12 11:41:38 +0300

    Change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ import backgroundImg from '@//assets/background1.png'
 
 export default function HomePage() {
     return (
-        <div className='w-full flex flex-col items-center justify-center bg-black'>
-            <div className='w-full relative'>
+        <div className='w-full flex flex-col items-center justify-center bg-white'>
+            <div className='w-full relative bg-black'>
                 <div className='w-full fixed top-0 left-0 z-20'>
                     <Header/>
                 </div>
