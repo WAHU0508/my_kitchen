b3a0ed1410ac921b6ecd842e64a945c79dae66ef
WAHU0508 <sharonwg0508@gmail.com>
2025-05-12 11:51:47 +0300

    Change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ import backgroundImg from '@//assets/background1.png'
 
 export default function HomePage() {
     return (
-        <div className='w-full flex flex-col items-center justify-center bg-white'>
-            <div className='w-full relative bg-black'>
+        <div className='w-full flex flex-col items-center justify-center bg-black'>
+            <div className='w-full relative'>
                 <div className='w-full fixed top-0 left-0 z-20'>
                     <Header/>
                 </div>
@@ -63,10 +63,10 @@ export default function HomePage() {
                 style={{
                     backgroundImage: `url(${backgroundImg.src})`
             }}>
-                <div className='bg-black w-[90%] md:w-[40%] lg:w-[40%] pl-[20px] md:pl-[50px] rounded-[20px]'>
+                <div className='bg-black w-[95%] md:w-[40%] lg:w-[40%] pl-[20px] md:pl-[50px] rounded-[20px]'>
                     <div className='border-l-2 border-white flex flex-row'>
                         <div className='border border-white mt-[50px] h-[100px]'></div>
-                        <div className='flex flex-col gap-[50px] lg:px-[100px] py-[50px]'>
+                        <div className='flex flex-col gap-[50px] px-[10px] md:px-[50px] lg:px-[100px] py-[50px]'>
                             <p className='text-[14px]'>Why Choose Alver Power</p>
                             <p className='text-[24px]'>Innovative Solutions For A Sustainable Future</p>
                             <p className='text-[16px]'>At Alver Power Systems, we adopt advanced manufacturing techniques and sustainable practices to create reliable and efficient power solutions. 
