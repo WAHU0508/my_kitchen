a88eb636b63120e6157c06879399f425d50f04d7
WAHU0508 <sharonwg0508@gmail.com>
2025-05-13 09:09:56 +0300

    Add Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
                         height={45}
                         className=''
                     />
-                    <div className='w-full grid grid-cols-2 space-x-4'>
+                    <div className='w-full grid grid-cols-2 space-x-4 space-y-6'>
                         <p className='text-[16px]'>About</p>
                         <p className='text-[16px]'>Tel: 0116004003</p>
                         <p className='text-[16px]'>Products | Services</p>
@@ -27,7 +27,45 @@ export default function Footer() {
                         <p className='text-[16px]'>Garden Estate rd, off Thika Road</p>
                     </div>
                 </div>
-                <div className='flex flex-col w-full md:w-[50%] items-center justify-center'></div>
+                <div className='flex flex-col w-full md:w-[50%] items-start'>
+                    <h1>Contact Us</h1>
+                    <form className='flex flex-col'>
+                        <div className='flex flex-row gap-2'>
+                            <div className='w-[50%] flex flex-col'>
+                                <label>First name*</label>
+                                    <input
+                                        type='text'
+                                        className='w-full border border-black px-2 py-2'
+                                        required
+                                    />
+                            </div>
+                            <div className='w-[50%] flex flex-col'>
+                                <label>Last name</label>
+                                    <input
+                                        type='text'
+                                        className='w-full border border-black px-2 py-2'
+                                    />
+                            </div>
+                        </div>
+                        <div className='w-full flex flex-col'>
+                            <label>Email*</label>
+                                <input
+                                    type='text'
+                                    className='w-full border border-black px-2 py-2'
+                                    required
+                                />
+                        </div>
+                        <div className='w-full flex flex-col'>
+                            <label>Email*</label>
+                                <textarea
+                                    placeholder="Your Message"
+                                    rows={5}
+                                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
+                                    required
+                                    />
+                        </div>
+                    </form>
+                </div>
             </div>
         </footer>
     )
