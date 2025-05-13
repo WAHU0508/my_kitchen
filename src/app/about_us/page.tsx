c8c0370c89e7813c61922f2fdedd520965aea252
WAHU0508 <sharonwg0508@gmail.com>
2025-05-13 15:15:58 +0300

    Absolute positioning

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -4,8 +4,11 @@ import React from 'react'
 
 function Aboutpage() {
   return (
-    <div className='w-full flex flex-col items-center justify-center'>
-      <Header/>
+    <div className='relative w-full flex flex-col items-center justify-center'>
+      <div className='w-full fixed top-0 left-0 z-20'>
+        <Header/>
+      </div>
+      
       <div className='w-full flex flex-col items-center justify-center text-black'>
         <h1 className='text-[32px] md:text-[40px]lg:text-[60px] xl:text-[64px]'>About Alver Power</h1>
         <p className='text-[16px] md:text-[20px]lg:text-[20px] xl:text-[24px]'>Alver Power Systems is a leading company specializing in switchboard manufacturing, 
