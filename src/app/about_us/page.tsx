225ab1462741af32944ccc52ff204554c3f74da0
WAHU0508 <sharonwg0508@gmail.com>
2025-05-13 15:17:05 +0300

    Absolute positioning

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -11,7 +11,7 @@ function Aboutpage() {
       
       <div className='w-full flex flex-col items-center justify-center text-black'>
         <h1 className='text-[32px] md:text-[40px]lg:text-[60px] xl:text-[64px]'>About Alver Power</h1>
-        <p className='text-[16px] md:text-[20px]lg:text-[20px] xl:text-[24px]'>Alver Power Systems is a leading company specializing in switchboard manufacturing, 
+        <p className='md:w-[60%] text-[16px] md:text-[20px] lg:text-[20px] xl:text-[24px]'>Alver Power Systems is a leading company specializing in switchboard manufacturing, 
           EPC services, solar solutions, electrical installation, and laser cutting machining.
            We are dedicated to providing high-quality, innovative solutions to meet the evolving needs of our clients.
         </p>
