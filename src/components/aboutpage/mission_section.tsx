b9362181ece5abacbddf52a4dd7765cfba8e3ae3
WAHU0508 <sharonwg0508@gmail.com>
2025-05-13 16:56:58 +0300

    Add border

--- src/components/aboutpage/mission_section.tsx
+++ src/components/aboutpage/mission_section.tsx
@@ -26,7 +26,7 @@ function MissionSection() {
                 <Image
                   src={mission}
                   alt='Mission Image'
-                  className='w-[60%] md:h-[350px] rounded-[20px]'
+                  className='w-full md:w-[90%] lg:w-[60%] h-auto mh:h-[400px] lg:h-[500px] rounded-[20px]'
                 />
               </div>
 
