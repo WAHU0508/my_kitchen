dd41f63a61455c483c425c984fff765e7c7aa557
WAHU0508 <sharonwg0508@gmail.com>
2025-05-13 11:26:37 +0300

    Import Link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState, useRef } from 'react';
 import { usePathname } from 'next/navigation';
-import Link from 'next/Link'
+import Link from 'next/link'
 import Image from 'next/image';
 import { Menu } from 'lucide-react';
 import logo from '@//assets/logo.png';
