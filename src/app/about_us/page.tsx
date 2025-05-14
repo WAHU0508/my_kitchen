1218ee5e884e1dbfd582ee2caca70f25fc3b5b35
WAHU0508 <sharonwg0508@gmail.com>
2025-05-14 10:23:57 +0300

    Add flex col

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -13,16 +13,21 @@ function Aboutpage() {
       
       <div className='w-full flex flex-col items-center justify-center text-black mt-[50px] md:mt-[60px] lg:mt-[100px]'>
         <h1 className='text-[32px] md:text-[40px]lg:text-[60px] xl:text-[64px]'>About Alver Power</h1>
-        <p className='border-b-2 border-black  md:w-[80%] lg:w-[50%] text-[12px] md:text-[16px] lg:text-[16px] xl:text-[20px] text-center'>Alver Power Systems is a leading company specializing in switchboard manufacturing, 
+        <p className='md:w-[80%] lg:w-[50%] text-[12px] md:text-[16px] lg:text-[16px] xl:text-[20px] text-center'>Alver Power Systems is a leading company specializing in switchboard manufacturing, 
           EPC services, solar solutions, electrical installation, and laser cutting machining.
            We are dedicated to providing high-quality, innovative solutions to meet the evolving needs of our clients.
         </p>
-        <div className='border-b-2 border-black w-full mt-[20px] md:mt-[20px]'>
+
+        <div className='w-full border-b-1 border-black'></div>
+        <div className='w-full mt-[20px] md:mt-[20px]'>
             <MissionSection/>
         </div>
-        <div className='border-b-2 border-black w-full flex items-center justify-center mt-[20px] md:mt-[20px]'>
+
+        <div className='w-full border-b-1 border-black'></div>
+        <div className='w-full flex items-center justify-center mt-[20px] md:mt-[20px]'>
             <OurJourney />
         </div>
+        <div className='w-full border-b-1 border-black'></div>
       </div>
 
       <Footer/>
--- src/components/aboutpage/our_journeu.tsx
+++ src/components/aboutpage/our_journeu.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function OurJourney() {
   return (
-    <div className='w-[95%] md:w-[90%] lg:w-[80%] flex items-center justify-center'>
+    <div className='w-[95%] md:w-[90%] lg:w-[80%] flex items-center justify-center flex-col'>
 
         {/* year one */}
-        <div className='w-full items-start'>
-            <div className='border border-black md:p-4 flex flex-col'>
+        <div className='w-full md:items-start'>
+            <div className='border-b border-black md:p-4 flex flex-col'>
                 <p className='text-[#C45308] lg:text-[24px] py-4 border border-black'>2017</p>
             </div>
         </div>
@@ -15,8 +15,8 @@ function OurJourney() {
         <div></div>
 
         {/* year two */}
-        <div className='w-full items-end'>
-            <div className='border border-black md:p-4 flex flex-col'>
+        <div className='w-full md:items-end'>
+            <div className='border-b border-black md:p-4 flex flex-col'>
                 <p className='text-[#C45308] lg:text-[24px] py-4 border border-black'>2017</p>
             </div>
         </div>
