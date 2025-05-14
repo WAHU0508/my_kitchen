56263b4c43498a47541dc6ed364e4cc3f5bfa11d
WAHU0508 <sharonwg0508@gmail.com>
2025-05-14 10:49:46 +0300

    Add separator

--- src/components/aboutpage/our_journeu.tsx
+++ src/components/aboutpage/our_journeu.tsx
@@ -6,7 +6,7 @@ function OurJourney() {
 
         {/* year one */}
         <div className='w-full flex md:items-start md:justify-start'>
-            <div className='border border-black md:p-4 flex flex-col md:w-[40%]'>
+            <div className='border border-black md:p-4 flex flex-col md:w-[40%] rounded-[20px]'>
                 <p className='text-[#C45308] lg:text-[24px] p-4 max-w-max border-b border-black'>2017</p>
             </div>
         </div>
@@ -23,7 +23,7 @@ function OurJourney() {
 
         {/* year two */}
         <div className='w-full flex md:items-end md:justify-end'>
-            <div className='border border-black md:p-4 flex flex-col md:w-[40%]'>
+            <div className='border border-black md:p-4 flex flex-col md:w-[40%] rounded-[20px]'>
                 <p className='text-[#C45308] lg:text-[24px] p-4 max-w-max border-b  border-black'>2022</p>
             </div>
         </div>
