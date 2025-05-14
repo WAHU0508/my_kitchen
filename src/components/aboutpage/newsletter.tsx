4bebf25d81e9d1f13067749773f76829c6e71bc5
WAHU0508 <sharonwg0508@gmail.com>
2025-05-14 15:14:39 +0300

    Edit newsletter

--- src/components/aboutpage/newsletter.tsx
+++ src/components/aboutpage/newsletter.tsx
@@ -4,11 +4,11 @@ function NewsLetter() {
   return (
     <div className='w-[95%] md:w-[90%] lg:w-[80%] flex items-center justify-center flex-col bg-black h-auto'>
         {/* text section */}
-        <div className='text-white border-l-2 border-white h-auto flex-flex-row gap-4'>
-            <div className='border-l-2 border-white mt-[20px] h-[50px]'></div>
+        <div className='text-white border-l-2 border-white h-auto flex flex-row gap-4'>
+            <div className='border-l-2 border-white mt-[20px] h-[30px]'></div>
             <div className='flex flex-col gap-6'>
-                <p className='text-[12px]'>News</p>
-                <p className='text-[16px]'>Alver Power in the Press</p>
+                <p className='text-[16px]'>News</p>
+                <p className='text-[20px]'>Alver Power in the Press</p>
             </div>
         </div>
     </div>
