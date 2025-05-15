d16f204e2730b67c83356231e2ceb7b2afba147e
WAHU0508 <sharonwg0508@gmail.com>
2025-05-15 13:50:55 +0300

    Add supabase functionality

--- src/app/api/subscribe/route.js
+++ src/app/api/subscribe/route.js
@@ -1,6 +1,6 @@
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '@//lib/supabaseClient';
 
-export async function POST(request: Request) {
+export async function POST(request) {
   const { email } = await request.json();
 
   if (!email) {
