564f897a1a8acda42e031bfb5cfaf2852708f494
WAHU0508 <sharonwg0508@gmail.com>
2025-05-21 08:51:41 +0300

    Edit hovering

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
     <header className={`w-full relative hover:bg-white hover:text-black flex items-center justify-center transition-all duration-500 ease-in-out ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
       {/* Header for large and medium screens */}
       <div
-        className={`hidden w-full md:flex md:flex-row items-center justify-between lg:px-8 h-[60px] sticky top-0 z-50 transition-all duration-500 ease-in-out 
+        className={`hidden w-full md:flex md:flex-row hover:bg-white hover:text-black items-center justify-between lg:px-8 h-[60px] sticky top-0 z-50 transition-all duration-500 ease-in-out 
           ${isScrolled ? 'bg-black/60 text-white hover:bg-white hover:text-black' : ''} 
           ${pathname === '/about_us' ? 'text-black' : 'text-white'}`}
       >
