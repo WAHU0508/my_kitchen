8f40e42dbd665616c5ad21c334aa65af5708ae61
WAHU0508 <sharonwg0508@gmail.com>
2025-05-21 09:13:28 +0300

    Add left margin

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -102,11 +102,11 @@ export default function Header() {
           </button> */}
         </nav>
 
-        <div className={`absolute top-full w-full left-0 bg-white text-black py-10 shadow-xl z-50 ${isHovered ? 'flex' : 'hidden'}`}
+        <div className={`absolute top-full w-full flex items-center justify-center text-black py-10 shadow-xl z-50 ${isHovered ? 'flex' : 'hidden'}`}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
       >
-        <div className="mx-auto w-full max-w-[1200px] px-8 flex justify-between">
+        <div className="mx-auto w-full max-w-[1200px] bg-white px-8 flex justify-between">
           <Link href="/overview" className="text-sm hover:text-[#C45308]">Overview</Link>
           {/* Add more items if needed */}
         </div>
