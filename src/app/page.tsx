111000af7dd828f6a579499caf3d94a9c4a43230
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 19:08:28 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Header from "@//components/header"
+// homepage
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function HomePage() {
-  return (
-    <div className="min-h-screen">
-      <Header />
-    </div>
-  )
+    return (
+        <div className='w-full flex flex-col items-center justify-center bg-white'>
+           {/*Header Section */}
+            <div className='w-full relative'>
+                <div className='w-full fixed top-0 left-0 z-20'>
+                    <Header/>
+                </div>
+                <Image
+                    src='/beautiful-rustic-kitchen-with-fresh-ingredients-an.png'
+                    className="w-full lg:min-h-screen"
+                />
+              <div
+        </div>
+    )
 }
