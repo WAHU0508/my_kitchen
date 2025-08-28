92a9f0777d40987f4bb3d9724da18851404d7769
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 21:12:54 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,8 +16,12 @@ export default function Header() {
           transition-all duration-500 ease-in-out
         "
       >
-        <Link href="/">
+        <Link href="/" className='flex flex-row gap-4'>
           <Image src={logo} alt="KitchenTik" width={50} height={45} />
+          <div className='flex flex-col text-white'>
+            <p className='text-[20px]'>KitchenTik</p>
+            <p className='text-[15px]'>Your kitchen, your sanctuary.</p>
+          </div>
         </Link>
         
         <nav className='flex flex-row items-center justify-center space-x-6 text-white md:text-[14px]'>
@@ -26,10 +30,10 @@ export default function Header() {
           <p className='hover:text-[#e46f2b]'>Recipes</p>
         </nav>
 
-        <button className='bg-blue text-white md:text-[10px] rounded-[10px] p-2'>
-            Subscribe
-          </button>
-        
+        <button className="bg-blue-600 hover:bg-blue-700 text-white md:text-[14px] rounded-[10px] px-4 py-2">
+          Subscribe
+        </button>
+  
         <div>
         </div>
       </div>
