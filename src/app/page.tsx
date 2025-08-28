0697530e998caf5b9382ca74c9ba29b41abf688e
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 22:47:21 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,10 +23,10 @@ export default function HomePage() {
                 {/* Header text */}
                 <div className="absolute inset-0 flex items-center justify-center">
                     <div className="text-center max-w-4xl px-6">
-                      <h1 className="text-md md:text-5xl lg:text-7xl font-bold text-white mb-6 drop-shadow-2xl text-balance">
+                      <h1 className="text-[12px] md:text-5xl lg:text-7xl font-bold text-white mb-6 md:drop-shadow-2xl text-balance">
                         Discover the joy of <span className='text-[#e46f2b]'>home cooking</span>
                       </h1>
-                      <p className="text-sm md:text-lg lg:text-xl text-white leading-relaxed drop-shadow-lg text-pretty">
+                      <p className="text-[9px] md:text-lg lg:text-xl text-white leading-relaxed md:drop-shadow-lg text-pretty">
                         Transform your kitchen into a culinary haven with our premium appliances and expert recipes. From
                         everyday meals to special occasions, we provide everything you need to create memorable dining
                         experiences for you and your loved ones.
