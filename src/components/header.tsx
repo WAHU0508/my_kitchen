c207b0b69101c6542e7ff533cc08c2a80dae1d16
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 20:09:48 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <header className='w-full relative flex flex-col items-center justify-center transition-all duration-500 ease-in-out'>
       <div
         className='hidden w-full md:flex md:flex-row items-center justify-between lg:px-8 h-[60px] sticky top-0 z-50 transition-all duration-500 ease-in-out 
-         bg-[#e46f2b]/5'
+         bg-[#e46f2b]/40'
       >
         <Link href='/'>
           <Image src={logo} alt="KitchenTik" width={50} height={45} />
