9f455cc50d0f7001a61b8157d77da43c0a581027
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 20:09:16 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,55 +1,13 @@
-'use client';
-import React, { useEffect, useState, useRef } from 'react';
-import { usePathname } from 'next/navigation';
+import React from 'react';
 import Link from 'next/link'
 import Image from 'next/image';
-import { Menu } from 'lucide-react';
 import logo from '@//assets/logo.png';
 
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [visible, setVisible] = useState(true);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const pathname = usePathname();
-
-  // Handle visibility on inactivity
-  useEffect(() => {
-    const resetTimer = () => {
-      if (!visible) setVisible(true);
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-      timeoutRef.current = setTimeout(() => {
-        setVisible(false);
-      }, 3000); // 3 seconds of inactivity
-    };
-
-    const events = ['mousemove', 'keydown', 'scroll', 'touchstart'];
-    events.forEach(event => window.addEventListener(event, resetTimer));
-
-    resetTimer(); // Start the timer on mount
-
-    return () => {
-      events.forEach(event => window.removeEventListener(event, resetTimer));
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    };
-  }, [visible]);
-
-   // Disable scroll when modal is open
-    useEffect(() => {
-        if (isOpen) {
-            document.body.style.overflow = 'hidden';
-            document.body.style.position = 'fixed';
-            document.body.style.width = '100%';
-        } else {
-            document.body.style.overflow = 'auto';
-            document.body.style.position = 'static';
-        }
-    }, [isOpen]);
 
   return (
-    <header className={`w-full relative flex flex-col items-center justify-center transition-all duration-500 ease-in-out ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      {/* Header for large and medium screens */}
+    <header className='w-full relative flex flex-col items-center justify-center transition-all duration-500 ease-in-out'>
       <div
         className='hidden w-full md:flex md:flex-row items-center justify-between lg:px-8 h-[60px] sticky top-0 z-50 transition-all duration-500 ease-in-out 
          bg-[#e46f2b]/5'
@@ -59,131 +17,6 @@ export default function Header() {
         </Link>
 
       </div>
-      
-
-      {/* header for smaller screens */}
-      <div className={`lg:hidden md:hidden flex flex-row w-full items-center justify-between bg-[#e46f2b]/60 px-2`}>
-        <button onClick={() => setIsOpen(true)}>
-            <Menu size={31} color='white'/>
-        </button>
-        <div className='flex items-center justify-center w-full'>
-            <Image
-                src={logo}
-                width={120}
-                height={38}
-                alt='Alver Power Systems logo'
-                className='w-[120px] h-[38px]'
-            />
-        </div>
-      </div>
-
-      {/* Modal and Overlay */}
-            {isOpen && (
-                <div className="fixed inset-0 z-50 flex">
-                    {/* Overlay */}
-                    <div
-                        className="fixed inset-0 bg-black opacity-40 transition-opacity duration-300"
-                        onClick={() => setIsOpen(false)}
-                    ></div>
-
-                    {/* Modal with slide-in animation */}
-                    <div
-                        className={`overflow-x-auto fixed right-0 top-0 h-screen w-[270px] flex flex-col items-center bg-white shadow-xl z-50 transition-transform duration-300 transform ${
-                            isOpen ? 'translate-x-0' : '-translate-x-full'
-                        }`}
-                        style={{ touchAction: 'none' }}
-                    >
-                        <div className='flex flex-col items-center'>
-                            <Image
-                                src={logo}
-                                width={120}
-                                height={38}
-                                alt='Alver Power Systems logo'
-                                className='w-[120px] h-[38px] mt-[22px] mb-[50px]'
-                            />
-                            <ul className="space-y-[50px]">
-                                <li><Link
-                                        href="/"
-                                        className={`border-b border-black/10 flex items-center justify-center w-[146px] h-[22px] text-[12px] ${
-                                          pathname === '/' ? 'text-red-600' : 'text-black'
-                                        }`}
-                                        onClick={() => setIsOpen(false)}
-                                        >
-                                        Home
-                                    </Link>
-                                </li>
-                                <li><Link
-                                        href="/about_us"
-                                        className={`border-b border-black/10 flex items-center justify-center w-[146px] h-[22px] text-[12px] ${
-                                          pathname === '/about_us' ? 'text-red-600' : 'text-black'
-                                        }`}
-                                        onClick={() => setIsOpen(false)}
-                                        >
-                                        About Us
-                                    </Link>
-                                </li>
-                                <li className="border-b border-black/10 flex flex-row items-center justify-center w-full">
-                                      <Link
-                                        href="/products_and_services"
-                                        className={`flex items-center justify-start w-[146px] h-[22px] text-[12px] ${
-                                          pathname === '/products_and_services' ? 'text-red-600' : 'text-black'
-                                        }`}
-                                        onClick={() => setIsOpen(false)}
-                                      >
-                                        Products & Services
-                                      </Link>
-                                    
-                                      {/* Plus/Minus button */}
-                                      <button
-                                        className="text-[16px] text-black ml-2"
-                                        onClick={(e) => {
-                                          e.preventDefault(); // prevent Link click
-                                          setIsExpanded(!isExpanded);
-                                        }}
-                                      >
-                                        {isExpanded ? '−' : '+'}
-                                      </button>
-                                    </li>
-                                {isExpanded && (
-                                  <ul className="pl-4 mt-2 space-y-2 text-[12px]">
-                                    <li>
-                                      <Link href="/products_and_services/solar_solutions" className="border-b border-black/10 p-1 flex items-center justify-center text-gray-700 hover:text-red-500">
-                                        Solar Solutions
-                                      </Link>
-                                    </li>
-                                    <li>
-                                      <Link href="/products_and_services/switchboard_manufacturing" className="border-b border-black/10 p-1 flex items-center justify-center text-gray-700 hover:text-red-500">
-                                        Switchboard Manufacturing
-                                      </Link>
-                                    </li>
-                                    <li>
-                                      <Link href="/products_and_services/CNC" className="border-b border-black/10 p-1 flex items-center justify-center text-gray-700 hover:text-red-500">
-                                        CNC
-                                      </Link>
-                                    </li>
-                                    <li>
-                                      <Link href="/products_and_services/electrical_installation" className="border-b border-black/10 p-1 flex items-center justify-center text-gray-700 hover:text-red-500">
-                                        Electrical Installation
-                                      </Link>
-                                    </li>
-                                  </ul>
-                                )}
-
-                                <li><Link
-                                        href="/contact_us"
-                                        className={`border-b border-black/10 flex items-center justify-center w-[146px] h-[22px] text-[12px] ${
-                                          pathname === '/contact_us' ? 'text-red-600' : 'text-black'
-                                        }`}
-                                        onClick={() => setIsOpen(false)}
-                                        >
-                                        Contact Us
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                        </div>
-                    </div>
-            )}
     </header>
   );
 }
