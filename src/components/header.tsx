7d1ebeb17e108d69070d0443c7cee4cfc8f6e734
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 20:34:35 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,8 +9,16 @@ export default function Header() {
   return (
     <header className='w-full relative flex flex-col items-center justify-center transition-all duration-500 ease-in-out'>
       <div
-        className='hidden w-full md:flex md:flex-row items-center justify-between lg:px-8 h-[60px] sticky top-0 z-50 transition-all duration-500 ease-in-out 
-         bg-[#e46f2b]/40'
+        className="
+          hidden md:flex 
+          w-full flex-row items-center justify-between 
+          lg:px-8 h-[60px] 
+          sticky top-0 z-50 
+          bg-[#e46f2b]/40 
+          backdrop-blur-md   /* blur background */
+          supports-[backdrop-filter]:bg-[#e46f2b]/40  /* ensure blur works */
+          transition-all duration-500 ease-in-out
+        "
       >
         <Link href='/'>
           <Image src={logo} alt="KitchenTik" width={50} height={45} />
