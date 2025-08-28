4671b9889742e177e4397dac852137d90cb64d76
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 14:00:46 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,14 @@ import React from "react"
 export default function Header() {
   return (
      <header className="w-full flex items-center justify-center">
-       <div className='w-full flex-items-center justify-center bg-[#e46f2b]/95 border-b border-black px-2'>
+       <div className='w-full flex flex-row items-center justify-between bg-[#e46f2b]/95 border-b border-black p-4 md:p-6'>
+        {/* icon */}
          <div className="h-8 w-8 rounded-full bg-white/20 flex items-center justify-center">
               <span className="text-white font-bold text-lg">🍳</span>
+               <p className='text-white font-bold text-md md:text-lg lg:text-xl'>Kitchen Tik</p>
+         </div>
+        
+         <div>
          </div>
        </div>
      </header>
