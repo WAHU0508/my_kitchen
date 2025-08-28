3156c7b44c6998370e4078f30f3f67cb7cc39dce
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 12:54:29 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,16 +4,12 @@ import { Utensils } from "lucide-react"
 
 export default function Header() {
   return (
-     <header className="w-full flex items-center justify-center border-b border-black bg-[#fffbeb]">
-        <div className="w-full flex items-center justify-center container mx-auto px-2 md:px-4 py-6">
-          <div className="w-full flex items-center justify-between border border-black">
-            <div className="flex items-center gap-[30px] border border-black">
-              <div className="bg-[#ea580c] text-white px-4 py-4 rounded-lg">
-                <Utensils className="h-[30px] w-[30px] stroke-[#ffffff]"/>
-              </div>
-          </div>
-          </div>
-        </div>
-      </header>
+     <header className="w-full flex items-center justify-center">
+       <div className='bg-[#e46f2b] border border-black px-2'>
+         <div className='bg-[#ea580c] p-4'>
+           <Utensils className='stroke-white'/>
+         </div>
+       </div>
+     </header>
   )
 }
