0cb78a253c146b7adf0e1fe06819933d2a858418
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-28 11:24:39 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-import Header from "@/components/header"
-// import HeroSection from "@/components/hero-section"
-// import FeaturedRecipes from "@/components/featured-recipes"
-// import CookingTips from "@/components/cooking-tips"
-// import Newsletter from "@/components/newsletter"
-// import Footer from "@/components/footer"
+import Header from "@//components/header;
 
 export default function HomePage() {
   return (
     <div className="min-h-screen">
       <Header />
-{/*       <main>
-        <HeroSection />
-        <FeaturedRecipes />
-        <CookingTips />
-        <Newsletter />
-      </main>
-      <Footer /> */}
     </div>
   )
 }
