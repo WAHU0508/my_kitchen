8c642044faf23e77b29b7204fcfd817f63f79842
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-29 17:14:21 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,11 +45,11 @@ export default function HomePage() {
                       </div>
                     </div>
                </div>
-               {/* browse categories */}
-                <div className='w-full flex items-center justify-center'>
-                    <h3 className="text-lg md:text-xl lg:text-3xl font-bold text-center text-black mb-12">Browse Categories</h3>
-                </div>
             </div>
+            {/* browse categories */}
+                <div className='w-full flex items-center justify-center'>
+                    <h3 className="text-lg md:text-xl lg:text-3xl font-bold text-center text-black mt-6 mb-12">Browse Categories</h3>
+                </div>
         </div>
     )
 }
