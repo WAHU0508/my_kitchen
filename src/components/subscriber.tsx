493bc1afa61fa1e15b8f53060dd132dc73a15d47
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-30 18:02:52 +0300

    Update subscriber.tsx

--- src/components/subscriber.tsx
+++ src/components/subscriber.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Subscribers() {
   return (
-    <div className='bg-[#cc7800] md:px-24 md:py-12 flex flex-col items-center justify-center rounded-lg'>
+    <div className='bg-[#cc7800] px-12 py-6 md:px-24 md:py-12 flex flex-col items-center justify-center rounded-lg'>
       <h2 className="text-3xl md:text-4xl font-bold mb-4 text-balance">Never Miss a Recipe</h2>
       <p className="white text-sm md:text-lg mb-8 max-w-2xl mx-auto text-pretty leading-relaxed text-center">
               Join our community of food lovers and get weekly recipes, cooking tips, and kitchen inspiration delivered
