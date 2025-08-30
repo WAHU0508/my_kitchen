570ca970fa15c3759fe332cf44fc8c4fbd05a346
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-08-30 17:59:56 +0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
   return (
     <div className='mt-6 w-full flex items-center justify-center bg-[#fff8ea] px-2 md:px-8 py-4'>
-      <p className="text-#3a3a3a text-sm">
+      <p className="text-[#3a3a3a] text-sm">
             &copy; 2024 Kitchen Chronicles. All rights reserved. Made with ❤️ for food lovers everywhere.
       </p>
     </div>
