a356312e6587c9452b256d07f1c813452dd13e51
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-07 00:48:55 +0300

    Update recipe.ts

--- src/sanity/schemaTypes/recipe.ts
+++ src/sanity/schemaTypes/recipe.ts
@@ -1,6 +1,6 @@
 import { defineType, defineField } from "sanity"
 
-export default const recipe = defineType({
+export default defineType({
   name: "recipe",
   title: "Recipe",
   type: "document",
