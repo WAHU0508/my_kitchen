82f8e1e437678d4e5ee21c03ebc7dced29bd6f69
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-06 21:05:33 +0300

    Update structure.ts

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,26 @@
-import type {StructureResolver} from 'sanity/structure'
+import { StructureResolver } from "sanity/structure"
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Big Appliances")
+        .child(S.documentTypeList("bigAppliance").title("Big Appliances")),
+
+      S.listItem()
+        .title("Small Appliances")
+        .child(S.documentTypeList("smallAppliance").title("Small Appliances")),
+
+      S.listItem()
+        .title("Recipes")
+        .child(S.documentTypeList("recipe").title("Recipes")),
+
+      // 👇 Include any default document types that aren’t explicitly handled above
+      ...S.documentTypeListItems().filter(
+        (listItem) =>
+          !["bigAppliance", "smallAppliance", "recipe"].includes(
+            listItem.getId() || ""
+          )
+      ),
+    ])
