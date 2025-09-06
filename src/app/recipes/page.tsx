78a3851e987f06fef164b6b55694e54995bb2fad
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-07 00:44:55 +0300

    Update page.tsx

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import { Star, Clock, Users, ChefHat, Search, Filter } from "lucide-react"
+import { Star, Clock, Users, ChefHat, Search } from "lucide-react"
 import Header from "@//components/header"
 import Footer from "@//components/footer"
 import { client } from "@//sanity/lib/client"
