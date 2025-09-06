23467dad8783849b6784c10553215a68e172bb28
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-06 22:59:15 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cdn.sanity.io"], // ✅ Allow Sanity-hosted images
+  },
 };
 
 export default nextConfig;
