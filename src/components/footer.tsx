7cbd1ba1f243b34af5d41794513b763f2d094e19
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-10 22:48:11 +0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import logo from "@//assets/logo.png"
-import { Instagram, Facebook, Pinterest, X } from "lucide-react"
+import { Instagram, Facebook, X } from "lucide-react"
 
 const PinterestIcon = () => (
   <svg
