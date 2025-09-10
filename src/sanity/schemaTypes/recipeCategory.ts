43eb2845460e0201c791748d171e2f2f9e85d6b2
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-10 21:58:21 +0300

    Update recipeCategory.ts

--- src/sanity/schemaTypes/recipeCategory.ts
+++ src/sanity/schemaTypes/recipeCategory.ts
@@ -1,39 +1,20 @@
-// ./schemas/recipeCategory.ts
-
-import { defineType } from "sanity"
+// schemas/bigApplianceCategory.ts
+import { defineType, defineField } from "sanity"
 
 export default defineType({
   name: "recipeCategory",
   title: "Recipe Category",
   type: "document",
   fields: [
-    {
+    defineField({
       name: "title",
       title: "Category Title",
       type: "string",
-      validation: (Rule) => Rule.required().min(2).max(50),
-    },
-    {
+    }),
+    defineField({
       name: "icon",
       title: "Icon",
-      type: "string",
-      description: "Emoji or icon to represent this category (e.g. 🍰, 🥗, 🍖)",
-    },
-    {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
-      options: {
-        source: "title",
-        maxLength: 96,
-      },
-    },
-    {
-      name: "description",
-      title: "Description",
-      type: "text",
-      rows: 3,
-      description: "Optional: Short description of this category",
-    },
+      type: "string", // Blogger can paste emoji or use text
+    }),
   ],
 })
