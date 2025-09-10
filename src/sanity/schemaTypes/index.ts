48a708675ed9955962d32b57955b720d638808ba
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-10 18:58:29 +0300

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import bigAppliance from "./bigAppliance"
 import bigApplianceCategory from "./bigApplianceCategory"
 import smallAppliance from "./smallAppliance"
+import smallApplianceCategory from "./smallApplianceCategory"
 import recipe from "./recipe"
 
-export const schemaTypes = [bigAppliance, smallAppliance, recipe, bigApplianceCategory]
+export const schemaTypes = [bigAppliance, smallAppliance, recipe, bigApplianceCategory, smallApplianceCategory]
