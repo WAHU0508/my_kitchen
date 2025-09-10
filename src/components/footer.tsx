9845d00d1a3fc6012e77741dea819e6529ee640a
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-10 22:40:50 +0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import logo from "@//assets/logo.png"
+import { Instagram, Facebook, Pinterest, X } from "lucide-react"
 
 export default function Footer() {
   return (
