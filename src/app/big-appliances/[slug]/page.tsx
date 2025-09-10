4f2564517600044f5a59675b7dd73ac58be50bd5
Sharon Gikenye <106180756+WAHU0508@users.noreply.github.com>
2025-09-10 21:06:50 +0300

    Update page.tsx

--- src/app/big-appliances/[slug]/page.tsx
+++ src/app/big-appliances/[slug]/page.tsx
@@ -39,7 +39,9 @@ export default async function ReviewPage({ params }: ReviewPageProps) {
       title,
       description,
       image,
-      category,
+      category->{
+        title
+      },
       date,
       rating,
       author,
